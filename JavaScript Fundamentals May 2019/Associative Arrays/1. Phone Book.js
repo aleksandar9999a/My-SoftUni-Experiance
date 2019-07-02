a5b706016db353262ee25cafd45939bfbfdb63ed
Aleksandar Todorov <aleksandar9999a@gmail.com>
2019-07-02 14:48:30 +0300

    PhoneBook Solved

--- JavaScript Fundamentals May 2019/Associative Arrays/1. Phone Book.js
+++ JavaScript Fundamentals May 2019/Associative Arrays/1. Phone Book.js
@@ -1,4 +1,19 @@
-function phoneBook(params) {
-    
+function phoneBook(arr) {
+    let bookWithNumber = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentPerson = arr[i].split(` `);
+        bookWithNumber[currentPerson[0]] = currentPerson[1];
+        
+    }
+
+    for (let person in bookWithNumber) {
+        console.log(person + ` -> ` + bookWithNumber[person]);
+        
+    }
 }
-phoneBook()
+phoneBook(['Tim 0834212554',
+'Peter 0877547887',
+'Bill 0896543112',
+'Tim 0876566344']
+)
