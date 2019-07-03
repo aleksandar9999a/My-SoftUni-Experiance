2eeb97c2304111875a89cc999ee63375d3c813d2
Aleksandar Todorov <aleksandar9999a@gmail.com>
2019-07-03 20:18:17 +0300

    Storage problem solved

--- JavaScript Fundamentals May 2019/Associative Arrays/2. Storage.js
+++ JavaScript Fundamentals May 2019/Associative Arrays/2. Storage.js
@@ -1,4 +1,27 @@
-function storage(params) {
-    
+function storage(items) {
+    let map = new Map();
+
+    for (const string of items) {
+        let currentItem = string.split(` `);
+        let product = currentItem[0];
+        let quantity = Number(currentItem[1]);
+
+        if (!map.has(product)) {
+            map.set(product, quantity);
+        }
+        else{
+            let currentQuantity = map.get(product);
+            let newQuantity = currentQuantity += quantity;
+            map.set(product, newQuantity);
+        }
+    }
+
+    for (const [product, quantity] of map) {
+        console.log(`${product} -> ${quantity}`);
+    }
 }
-storage()
+storage(['tomatoes 10',
+'coffee 5',
+'olives 100',
+'coffee 40']
+)
