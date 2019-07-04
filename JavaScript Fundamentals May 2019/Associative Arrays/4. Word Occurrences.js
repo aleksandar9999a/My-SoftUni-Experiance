d4f6b3b0cbf429bd16377ec19808ee4fc718ca58
Aleksandar Todorov <aleksandar9999a@gmail.com>
2019-07-04 14:30:14 +0300

    initialized Word Occurrences problem

---
+++ JavaScript Fundamentals May 2019/Associative Arrays/4. Word Occurrences.js
@@ -0,0 +1,4 @@
+function wordOccurrences(wordArr) {
+    
+}
+wordOccurrences(["Here", "is", "the", "first", "sentence", "Here", "is", "another", "sentence", "And", "finally", "the", "third", "sentence"])
