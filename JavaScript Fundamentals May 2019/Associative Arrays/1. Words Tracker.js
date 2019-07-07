5cb5feb2e97eced43419affdd4c9800e4bcbf404
aleksandar9999a <aleksandar9999a@gmail.com>
2019-07-07 20:21:13 +0300

    Initialize Words Tracker

---
+++ JavaScript Fundamentals May 2019/Associative Arrays/1. Words Tracker.js
@@ -0,0 +1,8 @@
+function wordsTracker(wordArr) {
+    
+}
+wordsTracker([
+    'this sentence', 'In','this','sentence','you','have','to','count','the','occurances','of','the'
+    ,'words','this','and','sentence','because','this','is','your','task'
+    ]
+    )
