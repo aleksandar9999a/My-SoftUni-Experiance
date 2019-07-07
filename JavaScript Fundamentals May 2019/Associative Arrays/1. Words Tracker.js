c32fc55a38f874e3113a17df939fa2dee162fdf6
aleksandar9999a <aleksandar9999a@gmail.com>
2019-07-07 20:31:56 +0300

    Finished with Words Tracker and start with Odd Occurrences

--- JavaScript Fundamentals May 2019/Associative Arrays/1. Words Tracker.js
+++ JavaScript Fundamentals May 2019/Associative Arrays/1. Words Tracker.js
@@ -1,8 +1,30 @@
 function wordsTracker(wordArr) {
-    
+    let map = new Map();
+    let wordForSearch = wordArr.shift().split(` `);
+
+    for (let i = 0; i < wordForSearch.length; i++) {
+        let currentWord = wordForSearch[i];
+        let wordCounter = 0;
+
+        for (let x = 0; x < wordArr.length; x++) {
+            let currentWordFromArr = wordArr[x];
+            
+            if (currentWord === currentWordFromArr) {
+                wordCounter++;
+            }
+        }
+        
+        map.set(currentWord, wordCounter);
+    }
+
+    let sorted = [...map].sort((a, b) => b[1] - a[1]);
+
+    for (const [word, count] of sorted) {
+        console.log(`${word} - ${count}`);
+    }
 }
 wordsTracker([
-    'this sentence', 'In','this','sentence','you','have','to','count','the','occurances','of','the'
-    ,'words','this','and','sentence','because','this','is','your','task'
+    'this sentence', 'In','this','this','you','sentence','to','count','this','occurances','of','the'
+    ,'sentence','this','and','sentence','because','this','is','your',''
     ]
     )
---
+++ JavaScript Fundamentals May 2019/Associative Arrays/2. Odd Occurrences.js
@@ -0,0 +1,4 @@
+function oddOccurrences(params) {
+    
+}
+oddOccurrences()
