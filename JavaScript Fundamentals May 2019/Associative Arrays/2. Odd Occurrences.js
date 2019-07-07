88376d5627858c716f1e3d3770be5c350c416735
aleksandar9999a <aleksandar9999a@gmail.com>
2019-07-07 20:56:11 +0300

    Odd Occurrences solved

--- JavaScript Fundamentals May 2019/Associative Arrays/2. Odd Occurrences.js
+++ JavaScript Fundamentals May 2019/Associative Arrays/2. Odd Occurrences.js
@@ -1,4 +1,24 @@
-function oddOccurrences(params) {
-    
+function oddOccurrences(words) {
+    let wordWithOddCounter = [];
+    let wordArr = words.split(` `);
+
+    while (wordArr.length > 0) {
+        let currentWord = wordArr.shift();
+        let counter = 1;
+
+        for (let i = 0; i < wordArr.length; i++) {
+            if (wordArr[i].toLowerCase() === currentWord.toLowerCase()) {
+                counter++;
+                wordArr.splice(i, 1);
+                i--;
+            }
+        }
+
+        if (counter % 2 != 0) {
+            wordWithOddCounter.push(currentWord.toLowerCase());
+        }
+    }
+
+    console.log(wordWithOddCounter.join(` `));
 }
-oddOccurrences()
+oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
