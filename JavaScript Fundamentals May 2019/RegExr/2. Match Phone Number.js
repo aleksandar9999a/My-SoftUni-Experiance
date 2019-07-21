017af5b9a0ddd9832ba0a04979adbc86b908ad77
aleksandar9999a <aleksandar9999a@gmail.com>
2019-07-21 10:49:18 +0300

    Match Phone Number problem solved

---
+++ JavaScript Fundamentals May 2019/RegExr/2. Match Phone Number.js
@@ -0,0 +1,12 @@
+function matchPhoneNumber(phonesArr) {
+    let pattern = /(?<!\d)[+]359([ -])2\1\d{3}\1\d{4}\b/g;;
+    let validNumbers = [];
+    let validNumber = null;
+
+    while ((validNumber = pattern.exec(phonesArr)) !== null) {
+        validNumbers.push(validNumber[0]);
+    }
+    
+    console.log(validNumbers.join(', '));
+}
+matchPhoneNumber([ '+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222' ])
