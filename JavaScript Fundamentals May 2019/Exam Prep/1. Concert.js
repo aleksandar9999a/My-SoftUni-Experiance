a752d557a6860093f79494881b40eae3a65d4476
aleksandar9999a <aleksandar9999a@gmail.com>
2019-07-29 16:36:35 +0300

    Start with Song Encryption
    Problem with validation for artist and song.

--- JavaScript Fundamentals May 2019/Exam Prep/1. Concert.js
+++ JavaScript Fundamentals May 2019/Exam Prep/1. Concert.js
@@ -58,10 +58,12 @@ function processInput(input) {
         }
     }
 }
-processInput([ 'Add; The Beatles; John Lennon, Paul McCartney',
-'Add; The Beatles; Paul McCartney, George Harrison',
-'Add; The Beatles; George Harrison, Ringo Starr',
-'Play; The Beatles; 3698',
-'Play; The Beatles; 3828',
+processInput([ 'Play; The Beatles; 2584',
+'Add; The Beatles; John Lennon, Paul McCartney, George Harrison, Ringo Starr',
+'Add; Eagles; Glenn Frey, Don Henley, Bernie Leadon, Randy Meisner',
+'Play; Eagles; 1869',
+'Add; The Rolling Stones; Brian Jones, Mick Jagger, Keith Richards',
+'Add; The Rolling Stones; Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Charlie Watts, Ian Stewart',
+'Play; The Rolling Stones; 4239',
 'start of concert',
-'The Beatles' ])
+'The Rolling Stones' ])
---
+++ JavaScript Fundamentals May 2019/Exam Prep/2. Song Encryption.js
@@ -0,0 +1,114 @@
+function songEncryption(songsArr) {
+    processingInput(songsArr);
+
+    function processingInput(input) {
+
+        for (let line of input) {
+            if (line === 'end') {
+                break;
+            }
+    
+            let [artist, song] = line.split(':');
+            if (artistValidation(artist) && artistValidation(song)) {
+                let encryptedArtist = encryptionArtist(artist);
+                let encryptedSong = encryptionSong(song, artist);
+                console.log(`Successful encryption: ${encryptedArtist}@${encryptedSong}`);
+            }
+            else{
+                console.log(`Invalid input!`);
+            }
+        }
+    }
+    
+    function artistValidation(artist) {
+        let artistPattern = /[A-Z][a-z]+/g;
+        let validArtist = null;
+    
+        while ((validArtist = artistPattern.exec(artist)) !== null) {
+            if (validArtist === artist) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    
+    function artistValidation(song) {
+        let songPattern = /[A-Z]+/g;
+        let validSong = null;
+    
+        while ((validSong = songPattern.exec(song)) !== null) {
+            console.log(validSong);
+            
+            if (validSong.join(' ') === song) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    
+    function encryptionArtist(artist) {
+        let encrypt = '';
+    
+        for (let i = 0; i < artist.length; i++) {
+            if (artist[i] === ' ') {
+                encrypt += ' ';
+            }
+            else if (artist[i] === `'`) {
+                encrypt += `'`;
+            }
+            else{
+                let asciiIndex = artist.charCodeAt(i) + artist.length;
+    
+                if (artist[i] === artist[i].toUpperCase() && asciiIndex > 90) {
+                    asciiIndex -= 25;
+                    encrypt += String.fromCharCode(asciiIndex);
+                }
+                else if (artist[i] === artist[i].toUpperCase() && asciiIndex <= 90) {
+                    encrypt += String.fromCharCode(asciiIndex);
+                }
+                else{
+                    if (asciiIndex > 122) {
+                        asciiIndex -= 25;
+                        encrypt += String.fromCharCode(asciiIndex);
+                    }
+                    else{
+                        encrypt += String.fromCharCode(asciiIndex);
+                    }
+                }
+            }
+        }
+    
+        return encrypt;
+    }
+    
+    function encryptionSong(song, artist) {
+        let encrypt = '';
+    
+        for (let i = 0; i < song.length; i++) {
+            if (song[i] === ' ') {
+                encrypt += ' ';
+            }
+            else{
+                let asciiIndex = song.charCodeAt(i) + artist.length;
+    
+                if (asciiIndex > 90) {
+                    asciiIndex -= 25;
+                    encrypt += String.fromCharCode(asciiIndex);
+                }
+                else if (asciiIndex <= 90) {
+                    encrypt += String.fromCharCode(asciiIndex);
+                }
+            }
+        }
+    
+        return encrypt;
+    }
+}
+
+//songEncryption([ 'Eminem:VENOM', 'Linkin park:NUMB', 'Drake:NONSTOP', 'Adele:HELLO', 'end' ])
+
+songEncryption([ 'Michael Jackson:ANOTHER PART OF ME', 'Adele:ONE AND ONLY', 'Guns n\'roses:NOVEMBER RAIN', 'Christina Aguilera: HuRt', 'end' ])
