091b12f1bc7ee5d250544322ec3e6688e729518a
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-04 21:30:16 +0300

    Next Article Solved
    Simple Web page who display articles when you click button. Closer Exercise

---
+++ JS Advanced/JS Advanced/Advanced Functions/5. Next Article/Resources/05. Next Article/next-article.js
@@ -0,0 +1,21 @@
+function getArticleGenerator(input){   
+	const div = document.getElementById('content');
+	let i = 0;
+
+	function nextArticle() {
+		if (i === input.length) {
+			return;
+		}
+
+		let html = `
+		<article>
+			<p>
+				${input[i++]}
+			</p>
+		</article>`;
+		
+		div.innerHTML += html;
+	}
+
+	return nextArticle;
+}
