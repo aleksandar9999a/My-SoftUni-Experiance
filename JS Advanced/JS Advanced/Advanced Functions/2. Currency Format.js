a401982c5f5c83ff02235026581f985af7595fd7
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-04 19:47:42 +0300

    Command Processor Solved

--- JS Advanced/JS Advanced/Advanced Functions/2. Currency Format.js
+++ JS Advanced/JS Advanced/Advanced Functions/2. Currency Format.js
@@ -1,6 +1,5 @@
-const currencyFormatter = require(`./formator`);
+const currencyFormatter = require(`./currencyFormatter`);
 const getDollar = require(`./currencyType`);
 let currency = getDollar(currencyFormatter);
 
 console.log(currency(100,5384));
-
