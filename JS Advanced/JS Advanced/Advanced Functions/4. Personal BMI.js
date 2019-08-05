49a4783cc6eaf7179f6b3263bc8326e537005ba1
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-05 20:46:51 +0300

    Personal BMI Solved

---
+++ JS Advanced/JS Advanced/Advanced Functions/4. Personal BMI.js
@@ -0,0 +1,42 @@
+function personalBMI(pName, pAge, pWeight, pHeight) {
+    let pI = {
+        age: pAge,
+        weight: pWeight,
+        height: pHeight
+    }
+
+    let person = {
+        name: pName,
+        personalInfo: pI,
+        BMI: Number(calculateBMI(pI).toFixed()),
+        status: chechStatus(calculateBMI(pI).toFixed())
+    }
+
+    function calculateBMI(personalInfo){
+        let myBMI = personalInfo[`weight`]/(personalInfo[`height`] * personalInfo[`height`]) * 10000;
+        return myBMI;
+    }
+
+    function chechStatus(myBMI){
+        if (myBMI < 18.5) {
+            return `underweight`;
+        }
+        else if (myBMI < 25) {
+            return `normal`
+        }
+        else if (myBMI < 30) {
+            return `overweight`;
+        }
+        else if (myBMI >= 30) {
+            return `obese`;
+        }
+    }
+
+    if (person[`status`] === `obese`) {
+        person[`recommendation`] = 'admission required';
+    }
+
+    return person;
+}
+personalBMI(`Peter`, 29, 75, 182)
+personalBMI(`Honey Boo Boo`, 9, 57, 137)
