6ffc3f0206c9f91edfc5ab1a26aad715542942c9
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-05 21:13:13 +0300

    Functional Sum Solved

--- JS Advanced/JS Advanced/Advanced Functions/3. Functional Sum.js
+++ JS Advanced/JS Advanced/Advanced Functions/3. Functional Sum.js
@@ -1,4 +1,13 @@
-function functionalSum() {
-    
-}
-functionalSum()
+let test = (function(){
+    let sum = 0;
+
+    return function add(number) {
+        sum += number;
+        add.toString = function(){
+            return sum;
+        }
+        return add
+    }
+})();
+
+console.log(test(1)(6)(-3));
