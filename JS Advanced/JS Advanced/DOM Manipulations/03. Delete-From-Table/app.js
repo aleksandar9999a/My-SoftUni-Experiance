e4928c479241f48bda1084eb9f0428da77e801ad
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-08 21:28:07 +0300

    Delete From Table Solved
    Added class in HTML for this problem. This doesn't work in judge.

--- JS Advanced/JS Advanced/DOM Manipulations/03. Delete-From-Table/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations/03. Delete-From-Table/app.js
@@ -1,3 +1,50 @@
-function deleteByEmail() {
-    console.log('TODO:...');
-}
+function deleteByEmail(){
+    const SELECTORS = {
+        email: '[name=email]',
+        rows: '#customers tbody tr',
+        emailCell: '.email-cell',
+        notification: '#result'
+    };
+    
+    const NOTIFICATION_TEXT = {
+        success: 'Deleted.',
+        error: 'Not found'
+    }
+    
+    const getEmail = () => document.querySelector(SELECTORS.email).value;
+    const getRows = () => document.querySelectorAll(SELECTORS.rows);
+    
+    const getRowToDelete = (rows, email) => 
+        [...rows].find(row => 
+            row.querySelector(SELECTORS.emailCell).innerHTML == email);
+    
+    const deleteRow = (row) => row.remove();
+    
+    const clearEmail = () => document.querySelector(SELECTORS.email).value = '';
+    
+    const setNotification = (text) => 
+            document.querySelector(SELECTORS.notification)
+            .innerHTML = text;
+    
+    const setSuccessfullNotification = () => {
+        setNotification(NOTIFICATION_TEXT.success);
+    };
+    
+    const setUnsuccessfullNotification = () => {
+        setNotification(NOTIFICATION_TEXT.error);
+    };
+
+    const email = getEmail();
+    const rows = getRows();
+    const rowToDelete = getRowToDelete(rows, email);
+
+    if (rowToDelete) {
+        deleteRow(rowToDelete);
+        setSuccessfullNotification();
+    }
+    else{
+        setUnsuccessfullNotification();
+    }
+
+    clearEmail();
+};
