f1622c79c4f0016ed913ed9f1f33fc1bc4a325d4
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-09 20:31:36 +0300

    Dynamic Validation Solved

--- JS Advanced/JS Advanced/DOM Manipulations/07. Dynamic-Validation/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations/07. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    const email = document.getElementById('email');
+    email.addEventListener('change', validation);
+
+    function validation() {
+        let email = this.value;
+        let pattern = /[a-z]+@[a-z]+.[a-z]+/g;
+
+        if (pattern.test(email)) {
+            this.classList.remove('error');
+        }
+        else{
+            this.classList.add('error')
+        }
+    }
 }
