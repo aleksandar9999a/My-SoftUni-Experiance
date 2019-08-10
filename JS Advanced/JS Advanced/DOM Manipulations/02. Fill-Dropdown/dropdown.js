d23d1b78f18e642c40d609f6df60dd00a4215c19
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-10 20:37:26 +0300

    Fill Dropdown Solved

--- JS Advanced/JS Advanced/DOM Manipulations/02. Fill-Dropdown/dropdown.js
+++ JS Advanced/JS Advanced/DOM Manipulations/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemText = document.getElementById('newItemText').value;
+    let itemValue = document.getElementById('newItemValue').value;
+    let option = document.createElement("OPTION");
+    option.textContent = itemText;
+    option.value = itemValue;
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
+
+    let menu = document.getElementById('menu');
+    menu.appendChild(option);
 }
