bccc2583a89ce29995143d28dcf3b347223a1272
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-10 21:23:31 +0300

    Sections Solved

--- JS Advanced/JS Advanced/DOM Manipulations/04. Sections/script.js
+++ JS Advanced/JS Advanced/DOM Manipulations/04. Sections/script.js
@@ -1,3 +1,22 @@
 function create(words) {
-   console.log('TODO:...');
+   
+   for (let i = 0; i < words.length; i++) {
+      let div = document.createElement('div');
+      let paragraph = document.createElement('p');
+      paragraph.textContent = words[i];
+      paragraph.style.display = 'none';
+      div.appendChild(paragraph);
+      div.addEventListener('click', showText);
+
+      document.getElementById('content').appendChild(div);
+   }
+
+   function showText() {
+      if (this.getElementsByTagName('p')[0].style.display = 'none') {
+         this.getElementsByTagName('p')[0].style.display = 'block';
+      }
+      else if (this.getElementsByTagName('p')[0].style.display = 'block') {
+         this.getElementsByTagName('p')[0].style.display = 'none';
+      }
+   }
 }
