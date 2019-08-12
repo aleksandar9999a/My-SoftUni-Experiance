18fb53f6be15517b682804184ec270de59392d9c
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-12 18:29:57 +0300

    Encode and Decode Messages Solved

--- JS Advanced/JS Advanced/DOM Manipulations/08. Encode-and-Decode-Messages/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,31 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const encodeBtn = document.getElementsByTagName('button')[0];
+    const decodeBtn = document.getElementsByTagName('button')[1];
+    encodeBtn.addEventListener('click', encode);
+    decodeBtn.addEventListener('click', decode);
+
+    function encode() {
+        let message = document.getElementsByTagName('textarea')[0].value;
+        let encodeMess = '';
+
+        for (let i = 0; i < message.length; i++) {
+            let charIndex = message[i].charCodeAt(0) + 1;
+            encodeMess += String.fromCharCode(charIndex);
+        }
+
+        document.getElementsByTagName('textarea')[1].value = encodeMess;
+        document.getElementsByTagName('textarea')[0].value = '';
+    }
+    
+    function decode() {
+        let message = document.getElementsByTagName('textarea')[1].value;
+        let decodeMess = '';
+
+        for (let i = 0; i < message.length; i++) {
+            let charIndex = message[i].charCodeAt(0) - 1;
+            decodeMess += String.fromCharCode(charIndex);
+        }
+
+        document.getElementsByTagName('textarea')[1].value = decodeMess;
+    }
 }
