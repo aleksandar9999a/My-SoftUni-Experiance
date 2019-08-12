eeb229d8569a14917f93017b3cfd6076fafe4056
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-12 16:16:25 +0300

    Locked Profile Solved

--- JS Advanced/JS Advanced/DOM Manipulations/07. Locked-Profile/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations/07. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    [...document.getElementsByTagName('button')].forEach(btn => btn.addEventListener('click', showInfo));
+
+    function showInfo(event){
+        let btn = event.target;
+        let currentProfile = btn.parentNode;
+        const lockStatus = currentProfile.querySelector('input[type="radio"]:checked').value;
+        
+        if (lockStatus === 'unlock') {
+            let id = currentProfile.getElementsByTagName('div')[0].id;
+            let currentContent = document.getElementById(id);
+
+            if (currentProfile.getElementsByTagName('button')[0].textContent === 'Show more') {
+                currentContent.style.display = "inline-block";
+                currentProfile.getElementsByTagName('button')[0].textContent = 'Hide it';
+            }
+            else {
+                currentContent.style.display = "none";
+                currentProfile.getElementsByTagName('button')[0].textContent = 'Show more';
+            }
+        }
+    }
 }
