d86f1e81fb5586cd0ee658f5f4c71131fa8d6e0d
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-22 16:20:20 +0300

    Remove unnecessary code

--- Bookmarker Application/js/main.js
+++ Bookmarker Application/js/main.js
@@ -1,12 +1,10 @@
 const SELECTOR = {
-    submitForm: '#submitForm',
     siteNameInput: '#siteName',
     siteURL: '#siteURL',
     submitBtn: '#submit',
     bookmarksDiv: '#bookmarksResults',
 }
 
-const submitForm = document.querySelector(SELECTOR.submitForm);
 const siteNameInput = document.querySelector(SELECTOR.siteNameInput);
 const siteURL = document.querySelector(SELECTOR.siteURL);
 const submitBtn = document.querySelector(SELECTOR.submitBtn);
