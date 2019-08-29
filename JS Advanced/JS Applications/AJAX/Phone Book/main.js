763cb679918a9e737fdc5ab200fafcc61c324594
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-29 15:36:29 +0300

    Added Validation for name, Notification and Upgraded Delete Function

--- JS Advanced/JS Applications/AJAX/Phone Book/main.js
+++ JS Advanced/JS Applications/AJAX/Phone Book/main.js
@@ -21,28 +21,78 @@ function addPerson() {
             phone: personPhone.value,
         };
 
-        if (localStorage.getItem('phoneBook') === null) {
-            phoneBook.push(currentPerson);
-            localStorage.setItem('phoneBook', JSON.stringify(phoneBook));
-        }
-        else {
-            phoneBook = JSON.parse(localStorage.getItem('phoneBook'));
-            let isContent = false;
-            for (let i = 0; i < phoneBook.length; i++) {
-                if (phoneBook[i].name === currentPerson.name) {
-                    isContent = true;
-                }
-            }
+        let isValid = validation(currentPerson);
 
-            if (!isContent) {
+        if (isValid) {
+            if (localStorage.getItem('phoneBook') === null) {
                 phoneBook.push(currentPerson);
                 localStorage.setItem('phoneBook', JSON.stringify(phoneBook));
             }
-        }
+            else {
+                phoneBook = JSON.parse(localStorage.getItem('phoneBook'));
+                let isContent = false;
 
-        clear();
-        printPersons();
+                for (let i = 0; i < phoneBook.length; i++) {
+                    if ((phoneBook[i].name.toLowerCase() === currentPerson.name.toLowerCase()) && (phoneBook[i].phone.toLowerCase() === currentPerson.phone.toLowerCase())) {
+                        isContent = true;
+                    }
+                }
+
+                if (!isContent) {
+                    phoneBook.push(currentPerson);
+                    localStorage.setItem('phoneBook', JSON.stringify(phoneBook));
+                }
+                else {
+                    notify('Already contained person with this name!');
+                }
+            }
+
+            clear();
+            printPersons();
+        }
     }
+}
+
+function validation(currentPerson) {
+    let validName = null;
+    let validPhone = null;
+
+    let namePattern = /[A-Za-z ]{4,}/g;
+    let phonePattern = /\d{10}/g;
+
+    let isValidName = false;
+    let isValidPhone = false;
+
+    while ((validName = namePattern.exec(currentPerson.name)) !== null) {
+        isValidName = true;
+    }
+
+    while ((validPhone = phonePattern.exec(currentPerson.phone)) !== null) {
+        isValidPhone = true;
+    }
+
+
+    if (isValidName && isValidPhone) {
+        return true;
+    }
+    else {
+        if (!isValidName) {
+            notify('Enter name with more characters or without digits!');
+        }
+        else if (!isValidPhone) {
+            notify('Enter phone number with more characters or without letters!')
+        }
+        return false;
+    }
+}
+
+function notify(message) {
+    document.getElementById('notification').textContent = message;
+    document.getElementById('notification').style.display = 'block';
+
+    setTimeout(function () {
+        document.getElementById('notification').style.display = 'none';
+    }, 2000)
 }
 
 function printPersons() {
@@ -52,16 +102,23 @@ function printPersons() {
     for (let i = 0; i < currentPhoneBook.length; i++) {
         let name = currentPhoneBook[i].name;
         let phone = currentPhoneBook[i].phone;
+        let myPerson = {
+            pName: name,
+            pPhone: phone,
+        }
+
         list.innerHTML += '<div>' +
-        '<h3>' + name + ': ' + phone +
-        ' <a onclick="deletePersons(\'' + name + '\')" class = "btn" href="#">Delete</a> ' +
-        '</h3>' + '</div>';
+            '<h3>' + name + ': ' + phone +
+            ' <a onclick="deletePersons(\'' + name + ' ' + phone  + '\')" class = "btn" href="#">Delete</a> ' +
+            '</h3>' + 
+            '</div>';
     }
 }
 
 function deletePersons(person) {
+    person = person.split(' ');
     let currentPhoneBook = JSON.parse(localStorage.getItem('phoneBook'));
-    currentPhoneBook = currentPhoneBook.filter(p => p.name != person);
+    currentPhoneBook = currentPhoneBook.filter(p => (p.name != person[0] || p.phone != person[1]));
     localStorage.setItem('phoneBook', JSON.stringify(currentPhoneBook));
     printPersons();
 }
