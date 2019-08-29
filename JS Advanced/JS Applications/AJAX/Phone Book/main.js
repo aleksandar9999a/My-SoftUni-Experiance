bf67ce7cf92ac2066623348bc28e5354d2501380
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-29 14:18:42 +0300

    Finish with simple design and JavaScript for Basic Function

--- JS Advanced/JS Applications/AJAX/Phone Book/main.js
+++ JS Advanced/JS Applications/AJAX/Phone Book/main.js
@@ -2,53 +2,71 @@ const SELECTORS = {
     name: '.person',
     phone: '.phone',
     submitBtn: '.submitBtn',
-    list: '.myBook',
+    list: '.contacts',
 };
 
 const btn = document.querySelector(SELECTORS.submitBtn);
-const name = document.querySelector(SELECTORS.name);
-const phone = document.querySelector(SELECTORS.phone);
+const personName = document.querySelector(SELECTORS.name);
+const personPhone = document.querySelector(SELECTORS.phone);
 const list = document.querySelector(SELECTORS.list);
 
 let phoneBook = [];
 
-btn.addEventListener('click', getPerson);
+btn.addEventListener('click', addPerson);
 
-function getPerson() {
-    if (name != '' && phone != '') {
+function addPerson() {
+    if (personName.value != '' && personPhone.value != '') {
         let currentPerson = {
-            name: name.value,
-            phone: phone.value,
+            name: personName.value,
+            phone: personPhone.value,
         };
 
-        console.log(currentPerson.phone);
-        
         if (localStorage.getItem('phoneBook') === null) {
             phoneBook.push(currentPerson);
             localStorage.setItem('phoneBook', JSON.stringify(phoneBook));
         }
         else {
             phoneBook = JSON.parse(localStorage.getItem('phoneBook'));
-            phoneBook.push(currentPerson);
-            localStorage.setItem('phoneBook', JSON.stringify(phoneBook));
+            let isContent = false;
+            for (let i = 0; i < phoneBook.length; i++) {
+                if (phoneBook[i].name === currentPerson.name) {
+                    isContent = true;
+                }
+            }
+
+            if (!isContent) {
+                phoneBook.push(currentPerson);
+                localStorage.setItem('phoneBook', JSON.stringify(phoneBook));
+            }
         }
+
+        clear();
+        printPersons();
     }
-
-    clear();
-    printPersons();
-}
-
-function clear() {
-    name.value = '';
-    phone.value = '';
 }
 
 function printPersons() {
     let currentPhoneBook = JSON.parse(localStorage.getItem('phoneBook'));
+    list.innerHTML = '';
 
     for (let i = 0; i < currentPhoneBook.length; i++) {
         let name = currentPhoneBook[i].name;
         let phone = currentPhoneBook[i].phone;
-        list.innerHTML += `<li> ${name}: ${phone}`;
+        list.innerHTML += '<div>' +
+        '<h3>' + name + ': ' + phone +
+        ' <a onclick="deletePersons(\'' + name + '\')" class = "btn" href="#">Delete</a> ' +
+        '</h3>' + '</div>';
     }
+}
+
+function deletePersons(person) {
+    let currentPhoneBook = JSON.parse(localStorage.getItem('phoneBook'));
+    currentPhoneBook = currentPhoneBook.filter(p => p.name != person);
+    localStorage.setItem('phoneBook', JSON.stringify(currentPhoneBook));
+    printPersons();
+}
+
+function clear() {
+    personName.value = '';
+    personPhone.value = '';
 }
