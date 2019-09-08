ddc5014bfd469b7cd3d4f26b35a56680d3a4073a
aleksandar9999a <aleksandar9999a@gmail.com>
2019-09-08 17:16:24 +0300

    Messenger Solved With Firebase

--- JS Advanced/JS Applications/AJAX/Messenger/main.js
+++ JS Advanced/JS Applications/AJAX/Messenger/main.js
@@ -1,1 +1,40 @@
-attachEvents();
+function attachEvents() {
+    const baseUrl = 'https://messenger-bc5a6.firebaseio.com/messages.json';
+    $('#submit').on('click', submitMessage);
+    $('#refresh').on('click', refreshTextBox);
+
+    function submitMessage() {
+        let author = $('#author').val();
+        let content = $('#content').val();
+        let timestamp = Date.now();
+
+        let message = {
+            author,
+            content,
+            timestamp
+        }
+
+        $.ajax({
+            url: baseUrl,
+            method: 'POST',
+            data: JSON.stringify(message),
+        });
+    }
+
+    function refreshTextBox() {
+        $.ajax({
+            url: baseUrl,
+            method: 'GET',
+            success: loadMessages
+        })
+    }
+
+    function loadMessages(data) {
+        let allMess = ''
+        Object.values(data).forEach(e => {
+            allMess += `${e.author}: ${e.content}\n`;
+        });
+
+        $('#messages').text(allMess);
+    }
+}
