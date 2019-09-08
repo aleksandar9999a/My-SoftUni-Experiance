727b853e3766c0690b52119fbedeec9b0c389d4a
aleksandar9999a <aleksandar9999a@gmail.com>
2019-09-08 16:03:25 +0300

    Finish with Bus Schedule

--- JS Advanced/JS Applications/AJAX/Bus Schedule/main.js
+++ JS Advanced/JS Applications/AJAX/Bus Schedule/main.js
@@ -1,8 +1,44 @@
 function solve() {
-    // TODO ...
+    let baseUrl = 'https://judgetests.firebaseio.com/schedule/';
+    let id = 'depot';
+
+    function depart() {
+        $.ajax({
+            url: baseUrl + id + '.json',
+            method: 'GET',
+            success: logStation
+        })
+
+        document.querySelector('#depart').disabled = true;
+        document.querySelector('#arrive').disabled = false;
+    }
+
+    function arrive() {
+        $.ajax({
+            url: baseUrl + id + '.json',
+            method: 'GET',
+            success: nextStop
+        })
+
+    }
+
+    function logStation(data) {
+        console.log(data);
+        document.querySelectorAll('.info')[0].textContent = `Next stop ${data.name}`;
+    }
+
+    function nextStop(data) {
+        console.log(data);
+        id = data.next;
+        document.querySelectorAll('.info')[0].textContent = `Arriving at ${data.name}`;
+        document.querySelector('#depart').disabled = false;
+        document.querySelector('#arrive').disabled = true;
+    }
+
     return {
         depart,
         arrive
     };
 }
+
 let result = solve();
