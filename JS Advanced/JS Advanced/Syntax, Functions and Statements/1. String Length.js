2f144d4469b340a7103fcada8f90cb99b284ee79
aleksandar9999a <aleksandar9999a@gmail.com>
2019-09-16 19:54:21 +0300

    String Length Solved

---
+++ JS Advanced/JS Advanced/Syntax, Functions and Statements/1. String Length.js
@@ -0,0 +1,6 @@
+function sumLength(strOne, strTwo, strThree) {
+    let sum = strOne.length + strTwo.length + strThree.length;
+    let avarageLength = Math.floor(sum/3);
+    console.log(sum);
+    console.log(avarageLength);
+}
