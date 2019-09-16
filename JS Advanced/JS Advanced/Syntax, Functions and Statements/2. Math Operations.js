719db336f3ba4cf802ffc5202734c87f20c8db24
aleksandar9999a <aleksandar9999a@gmail.com>
2019-09-16 20:01:51 +0300

    Math Operations Solved

---
+++ JS Advanced/JS Advanced/Syntax, Functions and Statements/2. Math Operations.js
@@ -0,0 +1,20 @@
+function calculator(num1, num2, operator) {
+    if (operator == '+') {
+        return num1 + num2;
+    }
+    else if (operator == '-') {
+        return num1 - num2;
+    }
+    else if (operator == '*') {
+        return num1 * num2;
+    }
+    else if (operator == '/') {
+        return num1 / num2;
+    }
+    else if (operator == '%') {
+        return num1 % num2;
+    }
+    else if (operator == '**') {
+        return num1 ** num2;
+    }
+}
