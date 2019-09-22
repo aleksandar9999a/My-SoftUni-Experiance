ebb4d6b1824cf318de3090f3edbd6bcbb6571b6b
aleksandar9999a <aleksandar9999a@gmail.com>
2019-09-22 20:39:13 +0300

    Better Look for Count Words in a Text

--- JS Advanced/JS Advanced/Objects/5. Count Words in a Text.js
+++ JS Advanced/JS Advanced/Objects/5. Count Words in a Text.js
@@ -1,14 +1,17 @@
-function wordsText(str) {
-    let data = str.match(/\w+/gim);
-    let obj = {};
-
-    for (let i = 0; i < data.length; i++) {
-        if (!obj.hasOwnProperty(data[i])) {
-            obj[data[i]] = 0;
-        }
-        obj[data[i]]++;
+function addPropIfMissing(a, b) {
+    if (!a.hasOwnProperty(b)) {
+        a[b] = 0;
     }
-
-    return obj;
+    return a
 }
-console.log(wordsText('JS devs use Node.js for server-side JS.-- JS for devs'));
+
+function wordsText2(str) {
+    return str
+        .match(/\w+/gim)
+        .reduce((a, b) => {
+            addPropIfMissing(a, b);
+            a[b]++;
+            return a;
+        }, {})
+}
+console.log(wordsText2('JS devs use Node.js for server-side JS.-- JS for devs'));
