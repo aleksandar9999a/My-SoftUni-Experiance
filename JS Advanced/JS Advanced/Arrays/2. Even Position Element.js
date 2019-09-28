715be2a2d9f21f3bb0dfefd4cd3e0e2f5046ca64
aleksandar9999a <aleksandar9999a@gmail.com>
2019-09-28 22:14:54 +0300

    Update 2. Even Position Element.js

--- JS Advanced/JS Advanced/Arrays/2. Even Position Element.js
+++ JS Advanced/JS Advanced/Arrays/2. Even Position Element.js
@@ -1,13 +1,5 @@
 function evenPositionElement(arr) {
-    let arrEvenNumbers = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if ((i + 1) % 2 != 0) {
-            arrEvenNumbers.push(arr[i]);
-        }
-    }
-
-    return arrEvenNumbers.join(' ');
+    return arr.filter((x, i) => i % 2 === 0).join(' ');
 }
 
 console.log(evenPositionElement([1, 2, 3]));
