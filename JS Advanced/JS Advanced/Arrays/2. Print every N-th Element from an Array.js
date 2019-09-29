8ec635aa1eeb192cdd31dc117274235df3d77e87
aleksandar9999a <aleksandar9999a@gmail.com>
2019-09-29 15:23:00 +0300

    Update 2. Print every N-th Element from an Array.js

--- JS Advanced/JS Advanced/Arrays/2. Print every N-th Element from an Array.js
+++ JS Advanced/JS Advanced/Arrays/2. Print every N-th Element from an Array.js
@@ -1,12 +1,6 @@
 function solve(data) {
     let step = Number(data.pop());
-    let arr = [];
-
-    for (let i = 0; i < data.length; i += step) {
-        arr.push(data[i]);
-    }
-
-    return arr.join('\n');
+    return data.filter((e, i) => i % step === 0).join('\n')
 }
 console.log(solve(['5', 
 '20', 
