2e753b6a8da2159fdd812d78aa416efe260344dc
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-02 14:30:25 +0300

    Update 2. Argument Info.js

--- JS Advanced/JS Advanced/Advanced Functions/2. Argument Info.js
+++ JS Advanced/JS Advanced/Advanced Functions/2. Argument Info.js
@@ -2,12 +2,12 @@ const printFormatterByType = x => `${typeof x}: ${x}`;
 const printFormatterNumber = x => `${x[0]} = ${x[1]}`;
 const ByDescendingOrder = (a, b) => b[1] - a[1];
 
-function argumentInfo2(...args) {
+function argumentInfo(...args) {
     let typeCounter = {};
     args.map(e => typeCounter[typeof e] ? typeCounter[typeof e]++ : typeCounter[typeof e] = 1);
     let arrByTypeCounts = Object.entries(typeCounter).sort(ByDescendingOrder).map(printFormatterNumber);
 
-    return args.map(printFormatterByType).concat(arrByTypeCounts).join('\n')
+    return args.map(printFormatterByType).concat(arrByTypeCounts).join('\n');
 }
+console.log(argumentInfo('cat', 42, function () { console.log('Hello world!'); }));
 
-console.log(argumentInfo2('cat', 42, function () { console.log('Hello world!'); }));
