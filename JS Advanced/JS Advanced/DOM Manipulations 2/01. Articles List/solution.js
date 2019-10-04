2a6e8bd2feb1fabb9e956269116976841c84578f
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-04 15:53:15 +0300

    Article List

--- JS Advanced/JS Advanced/DOM Manipulations 2/01. Articles List/solution.js
+++ JS Advanced/JS Advanced/DOM Manipulations 2/01. Articles List/solution.js
@@ -1,4 +1,28 @@
+const SELECTORS ={
+	articleList: '#articles',
+	title: '#createTitle',
+	content: '#createContent'
+}
+
+function createItem (title, content) {
+	return `<article>` +
+			`<h3>${title}</h3>` +
+			`<p>${content}</p>` +
+			`</article>`;
+}
+
+function clear() {
+	document.querySelector(SELECTORS.title).value = '';
+	document.querySelector(SELECTORS.content).value = '';
+}
+
 function createArticle() {
+	let title = document.querySelector(SELECTORS.title).value;
+	let content = document.querySelector(SELECTORS.content).value;
+
+	if (title != '' && content != '') {
+		document.querySelector(SELECTORS.articleList).innerHTML += (createItem(title, content));
+	}
 	
-	//TODO...
+	clear()
 }
