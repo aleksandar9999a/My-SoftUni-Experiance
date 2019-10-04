2a00165c9e59c37233fdb87fc6a924936f601a46
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-04 21:17:11 +0300

    Growing Word

--- JS Advanced/JS Advanced/DOM Manipulations 2/03. Growing Word/solution.js
+++ JS Advanced/JS Advanced/DOM Manipulations 2/03. Growing Word/solution.js
@@ -1,4 +1,24 @@
-function growingWord() {
+const colorMap = ["#5B88BD", "#A40014", "#8FF897"];
+let state = 0;
 
-  //TODO...
+function growingWord() {
+  let word = document.getElementById("growingText");
+
+  if (word === null) {
+    console.log("Ei kaltak nqma duma we");
+  }
+
+  let fontSize = window
+    .getComputedStyle(word)
+    .fontSize
+    .replace("px", "");
+  
+  if(state >= colorMap.length){
+    state = 0;
+  }
+
+  word.style.color = colorMap[state];
+  state++;
+  
+  word.style.fontSize = (fontSize === "0" ? "2" : fontSize * 2) + "px";
 }
