fa5d988666ca0877a7ce5118d67a75fdac446ee0
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-05 20:52:07 +0300

    Visited Websites

--- JS Advanced/JS Advanced/DOM Manipulations 2/04. Visited Websites/solution.js
+++ JS Advanced/JS Advanced/DOM Manipulations 2/04. Visited Websites/solution.js
@@ -1,4 +1,17 @@
-function solve() {
+const template = x => `visited ${x} times`;
 
-  //TODO...
+const links = {
+  "Google": 2,
+  "YouTube": 4,
+  "SoftUni": 1,
+  "stackoverflow": 6,
+  "Gmail": 7,
+  "Wikipedia": 4
 }
+
+document.addEventListener("click", (e) => {
+  if (e.target && e.target.parentNode && e.target.classList.contains("linkCta")) {
+    e.target.parentNode.querySelector(".linkOut")
+      .innerHTML = template(links[e.target.textContent.trim()]++)
+  }
+})
