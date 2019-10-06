e9c01b258863ca185cf800c2ec27252d73cfc379
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-06 16:09:15 +0300

    Order the Names Solution

--- JS Advanced/JS Advanced/DOM Manipulations 3/05. Order-the-Names/solution.js
+++ JS Advanced/JS Advanced/DOM Manipulations 3/05. Order-the-Names/solution.js
@@ -1,4 +1,16 @@
 function solve() {
+    let db = {};
+    let arrFromLi = [...document.getElementsByTagName("li")];
+    arrFromLi.map((x, i)=> db[String.fromCharCode(i + 65)] = [x.innerHTML.split(', ')]);
 
-    //TODO...
+    function addElements() {
+        let name = document.getElementsByTagName('input')[0].value.toLowerCase();
+        let capitalizeName = name.charAt(0).toUpperCase() + name.slice(1);
+        db[capitalizeName[0]].push(capitalizeName);
+        
+        arrFromLi.forEach((x, i) => x.innerHTML = db[String.fromCharCode(i + 65)].filter(e => e != "").join(', '));
+        document.getElementsByTagName('input')[0].value = '';
+    }
+
+    document.getElementsByTagName('button')[0].addEventListener('click', addElements);
 }
