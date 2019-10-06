be995f47a0dc2fdd4b896045680bed833345cb57
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-06 11:14:58 +0300

    JS Quizz Solution

--- JS Advanced/JS Advanced/DOM Manipulations 3/03. JavaScript-Quizz/solution.js
+++ JS Advanced/JS Advanced/DOM Manipulations 3/03. JavaScript-Quizz/solution.js
@@ -1,3 +1,50 @@
+let step = 0;
+let correctAnswerCounter = 0;
+
+const SELECTORS = {
+  question: ".answer-text",
+  finalRes: '#results',
+  btn: ".answer-wrap",
+  section: "section"
+}
+
+const answer = {
+  "onclick": true,
+  "onmouseclick": false,
+  "JSON.toString()": false,
+  "JSON.stringify()": true,
+  "A programming API for HTML and XML documents": true,
+  "The DOM is your source HTML": false
+}
+
+const addFunc = x => x.addEventListener('click', myAnswer);
+
+function myAnswer() {
+  let choice = this.querySelector(SELECTORS.question).textContent;
+  if (answer[choice]) {
+    correctAnswerCounter++;
+  }
+
+  return nextQuestion();
+}
+
+function nextQuestion() {
+  let sections = document.getElementsByTagName(SELECTORS.section);
+  sections[step].style.display = "none";
+  step++;
+  step <= 2 ? sections[step].style.display = "block" : winnerOrNot();
+}
+
+function winnerOrNot() {
+  document.querySelector(SELECTORS.finalRes).style.display = "block";
+  let h1 = document.querySelector(SELECTORS.finalRes).getElementsByTagName("h1")[0];
+
+  correctAnswerCounter === 3 ?
+  h1.innerHTML = 'You are recognized as top JavaScript fan!'
+  :
+  h1.innerHTML = `You have ${correctAnswerCounter} right answers`;
+}
+
 function solve() {
-  //TODO...
+  document.querySelectorAll(SELECTORS.btn).forEach(addFunc);
 }
