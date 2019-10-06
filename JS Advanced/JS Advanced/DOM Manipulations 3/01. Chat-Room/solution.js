a4809f1e03e0c8aada6d7118e7f7bd105821fb2c
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-06 09:51:30 +0300

    Chat Room Solution

--- JS Advanced/JS Advanced/DOM Manipulations 3/01. Chat-Room/solution.js
+++ JS Advanced/JS Advanced/DOM Manipulations 3/01. Chat-Room/solution.js
@@ -1,5 +1,26 @@
 function solve() {
-   //TODO...
+   const SELECTORS = {
+      sentBtn: '#send',
+      textarea: '#chat_input',
+      chatDiv: '#chat_messages',
+      chatInput: '#chat_input'
+   }
+
+   function clear(){
+      document.querySelector(SELECTORS.chatInput).value = "";
+   }
+
+   function addMessages(){
+      let text = document.querySelector(SELECTORS.textarea);
+      let chatDiv = document.querySelector(SELECTORS.chatDiv);
+   
+      let newMessage = document.createElement("div");
+      newMessage.className = "message my-message";
+      newMessage.innerHTML = text.value;
+      chatDiv.appendChild(newMessage);
+   
+      clear();
+   }
+
+   document.querySelector(SELECTORS.sentBtn).addEventListener('click', addMessages);
 }
-
-
