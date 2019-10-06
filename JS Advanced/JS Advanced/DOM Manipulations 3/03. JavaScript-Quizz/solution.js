1eba14434c759655402d8dd32a538463badce68f
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-06 11:29:42 +0300

    JS Quizz For SoftUni Judge

--- JS Advanced/JS Advanced/DOM Manipulations 3/03. JavaScript-Quizz/solution.js
+++ JS Advanced/JS Advanced/DOM Manipulations 3/03. JavaScript-Quizz/solution.js
@@ -1,50 +1,50 @@
-let step = 0;
-let correctAnswerCounter = 0;
+function solve() {
+  let step = 0;
+  let correctAnswerCounter = 0;
 
-const SELECTORS = {
-  question: ".answer-text",
-  finalRes: '#results',
-  btn: ".answer-wrap",
-  section: "section"
-}
-
-const answer = {
-  "onclick": true,
-  "onmouseclick": false,
-  "JSON.toString()": false,
-  "JSON.stringify()": true,
-  "A programming API for HTML and XML documents": true,
-  "The DOM is your source HTML": false
-}
-
-const addFunc = x => x.addEventListener('click', myAnswer);
-
-function myAnswer() {
-  let choice = this.querySelector(SELECTORS.question).textContent;
-  if (answer[choice]) {
-    correctAnswerCounter++;
+  const SELECTORS = {
+    question: ".answer-text",
+    finalRes: '#results',
+    btn: ".answer-wrap",
+    section: "section"
   }
 
-  return nextQuestion();
-}
+  const answer = {
+    "onclick": true,
+    "onmouseclick": false,
+    "JSON.toString()": false,
+    "JSON.stringify()": true,
+    "A programming API for HTML and XML documents": true,
+    "The DOM is your source HTML": false
+  }
 
-function nextQuestion() {
-  let sections = document.getElementsByTagName(SELECTORS.section);
-  sections[step].style.display = "none";
-  step++;
-  step <= 2 ? sections[step].style.display = "block" : winnerOrNot();
-}
+  const addFunc = x => x.addEventListener('click', myAnswer);
 
-function winnerOrNot() {
-  document.querySelector(SELECTORS.finalRes).style.display = "block";
-  let h1 = document.querySelector(SELECTORS.finalRes).getElementsByTagName("h1")[0];
+  function myAnswer() {
+    let choice = this.querySelector(SELECTORS.question).textContent;
+    if (answer[choice]) {
+      correctAnswerCounter++;
+    }
 
-  correctAnswerCounter === 3 ?
-  h1.innerHTML = 'You are recognized as top JavaScript fan!'
-  :
-  h1.innerHTML = `You have ${correctAnswerCounter} right answers`;
-}
+    return nextQuestion();
+  }
 
-function solve() {
-  document.querySelectorAll(SELECTORS.btn).forEach(addFunc);
+  function nextQuestion() {
+    let sections = document.getElementsByTagName(SELECTORS.section);
+    sections[step].style.display = "none";
+    step++;
+    step <= 2 ? sections[step].style.display = "block" : winnerOrNot();
+  }
+
+  function winnerOrNot() {
+    document.querySelector(SELECTORS.finalRes).style.display = "block";
+    let h1 = document.querySelector(SELECTORS.finalRes).getElementsByTagName("h1")[0];
+
+    correctAnswerCounter === 3 ?
+      h1.innerHTML = 'You are recognized as top JavaScript fan!'
+      :
+      h1.innerHTML = `You have ${correctAnswerCounter} right answers`;
+  }
+
+  Array.from(document.querySelectorAll(SELECTORS.btn)).forEach(addFunc);
 }
