e19bf7addafb9ea095a1a8e73cb6bd01b9e6ce31
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-07 19:26:26 +0300

    List Of Items Solution

--- JS Advanced/JS Advanced/DOM Manipulations 4/01. List-Of-Items/app.js
+++ JS Advanced/JS Advanced/DOM Manipulations 4/01. List-Of-Items/app.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const SELECTORS ={
+        list: '#items',
+        input: '#newItemText'
+    }
+
+    let text = document.querySelector(SELECTORS.input);
+    let li = document.createElement('li');
+    li.innerHTML = text.value;
+    document.querySelector(SELECTORS.list).appendChild(li);
+
+    text.value = '';
 }
