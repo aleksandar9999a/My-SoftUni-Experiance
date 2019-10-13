7f57a8f0f2ac64fa4c5151e9a598e5af6d2cac77
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-13 10:14:37 +0300

    DomElement & DomP for Render Paragraphs

--- JS Advanced/JS Advanced/Advanced Functions/JSON Test/app2.js
+++ JS Advanced/JS Advanced/Advanced Functions/JSON Test/app2.js
@@ -1,8 +1,50 @@
 import { MOCK } from "./MOCK_DATA.js";
 
+class DomElement{
+    _tag;
+    _content;
+    static _domFactory;
+
+    constructor(tag, content){
+        this._tag = tag;
+        this._content = content;
+    }
+
+    static get domFactory(){
+        return DomElement._domFactory;
+    }
+
+    static set domFactory(x){
+        DomElement._domFactory = x;
+    }
+
+    render(){
+        const e = DomElement.domFactory(this._tag);
+        e.innerHTML = this._content;
+        return e;
+    }
+}
+
+class DomP extends DomElement{
+    constructor(content){
+        super("p", content);
+    }
+}
+
 class Main {
-    handleEvent(){
-        console.log('pedal');
+    handleEvent(e){
+        let keys = Object.keys(MOCK[0]);
+        let html = "";
+
+        DomElement.domFactory = document.createElement.bind(document);
+
+        html += "<table>";
+        html += "<tr>";
+        html += keys.map(x => `<th>${x}</th>`).join('');
+        html += "</tr>";
+        html +="</table>";
+
+        document.all.app.appendChild(new DomP('test').render());
         
     }
 }
