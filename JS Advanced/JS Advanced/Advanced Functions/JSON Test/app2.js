111cb7e3708ef9404fc7ec8cfbf160ddcdf6b86b
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-13 09:49:12 +0300

    Make Main Class

---
+++ JS Advanced/JS Advanced/Advanced Functions/JSON Test/app2.js
@@ -0,0 +1,10 @@
+import { MOCK } from "./MOCK_DATA.js";
+
+class Main {
+    handleEvent(){
+        console.log('pedal');
+        
+    }
+}
+
+document.addEventListener("DOMContentLoaded", new Main())
