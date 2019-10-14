89f23e6159ef41e4aeda0b17afc903eb77e8b522
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-14 12:10:24 +0300

    Added Functions GenerateElement & CreatePendingQuest

--- JS Advanced/JS Advanced/Demo Exam/SoftDo/scripts/index.js
+++ JS Advanced/JS Advanced/Demo Exam/SoftDo/scripts/index.js
@@ -10,15 +10,51 @@ function getQuestion() {
     return document.getElementsByTagName('textarea')[0].value;
 }
 
+function generateElement(type, content, attrName, attrValue) {
+    let e = document.createElement(type);
+    if (typeof content === "string") {
+        e.innerHTML = content;
+    }
+    if (typeof content === "object") {
+        e.appendChild(content);
+    }
+    if (attrName !== '' && attrValue !== '' && typeof attrName === 'string' && typeof attrValue === 'string') {
+        e.setAttribute(attrName, attrValue);
+    }
+    return e;
+}
 
+function createPendingQuest(user, question) {
+    let pendingDiv = generateElement('div', undefined, "class", "pendingQuestion");
+    let img = generateElement('img');
+    let span = generateElement('span', user);
+    let p = generateElement('p', question);
+    let actionDiv = generateElement('div', undefined, "class", "actions");
+    let openBtn = generateElement('button', 'Open', "class", "open");
+    let archiveBtn = generateElement('button', 'Archive', "class", "archive");
+    
+    img.src = "./images/user.png";
+    img.setAttribute("width", "32");
+    img.setAttribute("height", "32");
+    
+    actionDiv.appendChild(archiveBtn);
+    actionDiv.appendChild(openBtn);
+    pendingDiv.appendChild(img);
+    pendingDiv.appendChild(span);
+    pendingDiv.appendChild(p);
+    pendingDiv.appendChild(actionDiv);
+
+    return pendingDiv;
+}
 
 function createQuestion(){
     const question = getQuestion();
 
     if (question !== '') {
         const userName = getUserName();
-        console.log(question);
-        
+        const newQuest = createPendingQuest(userName, question)
+        const outputSection = document.getElementById('pendingQuestions');
+        outputSection.appendChild(newQuest);
     }
     
     
