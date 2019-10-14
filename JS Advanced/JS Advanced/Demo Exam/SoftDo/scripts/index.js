0766fd910803062c2432a17a835b8c8bcc2d3599
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-14 16:18:09 +0300

    Added Templates, Selectors, Optimized Functions

--- JS Advanced/JS Advanced/Demo Exam/SoftDo/scripts/index.js
+++ JS Advanced/JS Advanced/Demo Exam/SoftDo/scripts/index.js
@@ -1,10 +1,42 @@
-const getQuestion = () => document.getElementsByTagName('textarea')[0].value;
+const SELECTORS = {
+    sendBtn: "#send",
+    textarea: "#myText",
+    userName: "#userName",
+    openQuestion: "#openQuestions",
+    pendingQuestions: "#pendingQuestions",
+    replyOl: ".reply",
+    replyInput: ".replyInput",
+    replySection: ".replySection"
+}
+
+const pendingDivTemplate = {
+    pendingDiv: ['div', undefined, "class", "pendingQuestion"],
+    actionDiv: ['div', undefined, "class", "actions"],
+    btn: [['button', 'Archive', "class", "archive", removeElement],['button', 'Open', "class", "open", createOpenQuestion]],
+    img: ['img', undefined, ["width", "height"], ["32", "32"]]
+}
+
+const openQuestionDivTemplate = {
+    openQuestionDiv: ['div', undefined, "class", "openQuestion"],
+    actionDiv: ['div', undefined, "class", "actions"],
+    btn: [['button', 'Reply', "class", "reply", replyToTheQuestion]],
+    img: ['img', undefined, ["width", "height"], ["32", "32"]]
+}
+
+const replyDivTemplate = {
+    replyDiv: ["div", undefined, ["class"], ["replySection"]],
+    input: ["input", undefined, ["class", "type", "placeholder"], ["replyInput", "text", "Reply to this question here..."]],
+    btn: ["button", "Send", "class", "replyButton", sendReply],
+    ol: ["ol", undefined, ["class", "type"], ["reply", "1"]]
+}
+
+const getQuestion = () => document.querySelector(SELECTORS.textarea).value;
 const generateMultipleButtons = (params) => generateElement(params[0], params[1], params[2], params[3], params[4]);
 const appendElements = (parent, elements) => elements.map(x => parent.appendChild(x));
 const getParent = e => e.parentElement.parentElement;
 
 function getUserName() {
-    const name = document.getElementsByTagName('input')[0];
+    const name = document.querySelector(SELECTORS.userName);
     if (name.value === '') {
         return 'Anonymous';
     }
@@ -43,7 +75,7 @@ function removeElement() {
 
 function replyToTheQuestion() {
     const currDiv = getParent(this);
-    const replySection = currDiv.getElementsByClassName("replySection")[0];
+    const replySection = currDiv.querySelector(SELECTORS.replySection);
     const btn = currDiv.getElementsByTagName("button")[0];
 
     if (replySection.style.display === "block") {
@@ -55,55 +87,58 @@ function replyToTheQuestion() {
     }
 }
 
-function moveToOpenQuestions() {
-    const currDiv = getParent(this);
-    const openQuestion = document.getElementById('openQuestions');
-
-    const movedQuestion = createNewQuest(
-        currDiv.getElementsByTagName('span')[0].innerHTML,
-        currDiv.getElementsByTagName('p')[0].innerHTML,
-        ['div', undefined, "class", "openQuestion"],
-        ['div', undefined, "class", "actions"],
-        [['button', 'Reply', "class", "reply", replyToTheQuestion]]
+function createOpenQuestion() {
+    const pendingQuestion = getParent(this);
+    const questionDiv = createNewQuest(
+        openQuestionDivTemplate.openQuestionDiv,
+        openQuestionDivTemplate.actionDiv,
+        openQuestionDivTemplate.btn,
+        openQuestionDivTemplate.img,
+        ['span', pendingQuestion.getElementsByTagName('span')[0].innerHTML],
+        ['p', pendingQuestion.getElementsByTagName('p')[0].innerHTML]
     )
 
-    openQuestion.appendChild(movedQuestion);
-    currDiv.remove();
+    moveFromPendingToOpenQuestions(pendingQuestion, questionDiv)
+}
+
+function moveFromPendingToOpenQuestions(pendingQuestion, openQuestion) {
+    const openQuestions = document.querySelector(SELECTORS.openQuestion);
+    openQuestions.appendChild(openQuestion);
+    pendingQuestion.remove();
 }
 
 function sendReply() {
-    const ol = this.parentElement.getElementsByClassName("reply")[0];
-    const li = generateElement("li", this.parentElement.getElementsByTagName("input")[0].value);
+    const ol = this.parentElement.querySelector(SELECTORS.replyOl);
+    const li = generateElement("li", this.parentElement.querySelector(SELECTORS.replyInput).value);
     ol.appendChild(li);
 }
 
 function createReplyDiv() {
-    const replyDiv = generateElement("div", undefined, ["class"], ["replySection"]);
-    const input = generateElement("input", undefined, ["class", "type", "placeholder"], ["replyInput", "text", "Reply to this question here..."]);
-    const btn = generateElement("button", "Send", "class", "replyButton", sendReply);
-    const ol = generateElement("ol", undefined, ["class", "type"], ["reply", "1"]);
+    const replyDiv = generateElement(...replyDivTemplate.replyDiv);
+    const input = generateElement(...replyDivTemplate.input);
+    const btn = generateElement(...replyDivTemplate.btn);
+    const ol = generateElement(...replyDivTemplate.ol);
 
     replyDiv.style.display = "none";
     appendElements(replyDiv, [input, btn, ol]);
     return replyDiv;
 }
 
-function createNewQuest(user, question, divParams, secDivParams, btnParams) {
-    const pendingDiv = generateElement(divParams[0], divParams[1], divParams[2], divParams[3]);
-    const actionDiv = generateElement(secDivParams[0], secDivParams[1], secDivParams[2], secDivParams[3]);
-
-    const img = generateElement('img', undefined, ["width", "height"], ["32", "32"]);
-    img.src = "./images/user.png";
-    const span = generateElement('span', user);
-    const p = generateElement('p', question);
+function createNewQuest(divParams, secDivParams, btnParams, imgParams, spanParams, questionParams) {
+    const pendingDiv = generateElement(...divParams);
+    const actionDiv = generateElement(...secDivParams);
+    const img = generateElement(...imgParams);
+    const span = generateElement(...spanParams);
+    const p = generateElement(...questionParams);
     const btns = btnParams.map(generateMultipleButtons);
+
+    img.src = "./images/user.png";
 
     appendElements(actionDiv, btns)
     appendElements(pendingDiv, [img, span, p, actionDiv]);
 
     if (btnParams[0][3] === 'reply') {
-        const replyDiv = createReplyDiv();
-        pendingDiv.appendChild(replyDiv);
+        pendingDiv.appendChild(createReplyDiv());
     }
 
     return pendingDiv;
@@ -115,21 +150,17 @@ function createValidQuestion() {
     if (question !== '') {
         const userName = getUserName();
         const newQuest = createNewQuest(
-            userName,
-            question,
-            ['div', undefined, "class", "pendingQuestion"],
-            ['div', undefined, "class", "actions"],
-            [
-                ['button', 'Archive', "class", "archive", removeElement], 
-                ['button', 'Open', "class", "open", moveToOpenQuestions]
-            ]
+            pendingDivTemplate.pendingDiv,
+            pendingDivTemplate.actionDiv,
+            pendingDivTemplate.btn,
+            pendingDivTemplate.img,
+            ['span', userName],
+            ['p', question]
         );
 
-        const outputSection = document.getElementById('pendingQuestions');
+        const outputSection = document.querySelector(SELECTORS.pendingQuestions);
         outputSection.appendChild(newQuest);
     }
 }
 
-function mySolution() {
-    document.getElementsByTagName("button")[0].addEventListener('click', createValidQuestion)
-}
+document.querySelector(SELECTORS.sendBtn).addEventListener('click', createValidQuestion);
