b5a4e22f55af9884dab36b04776b671e0aa0b03b
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-16 15:11:13 +0300

    String Builder test on 90%

--- JS Advanced/JS Advanced/Unit Testing 2/String-Builder-test.js
+++ JS Advanced/JS Advanced/Unit Testing 2/String-Builder-test.js
@@ -10,4 +10,54 @@ describe('StringBuilder', () => {
         let expected = new StringBuilder('Test');
         expect(expected._stringArray).to.have.lengthOf(4);
     })
+
+    it('Function attached to instance', () => {
+        let expected = new StringBuilder('Test');
+        
+        expect(expected).to.have.property('append');
+        expect(expected).to.have.property('prepend');
+        expect(expected).to.have.property('insertAt');
+        expect(expected).to.have.property('remove');
+        expect(expected).to.have.property('toString');
+    })
+
+    it('Converts the passed in string argument to an array and adds it to the end of the storage (append)', () => {
+        let expected = new StringBuilder('Test');
+        expected.append('Demo')
+        let lastI = expected._stringArray.length;
+        expect(expected._stringArray.slice(lastI - 4, lastI).join('')).to.be.equal('Demo');
+    })
+
+    it('Converts the passed in string argument to an array and adds it to the beginning of the storage (prepend)', () => {
+        let expected = new StringBuilder('Test');
+        expected.prepend('Demo');
+        expect(expected._stringArray.slice(0, 4).join('')).to.be.equal('Demo');
+    })
+
+    it('Converts the passed in string argument to an array and adds it at the given index (insertAt)', () => {
+        let expected = new StringBuilder('Test');
+        expected.insertAt('Demo' , 2);
+        expect(expected._stringArray.slice(2, 6).join('')).to.be.equal('Demo');
+    })
+    
+
+    it('Removes elements from the storage, starting at the given index (inclusive), length number of characters (remove)', () => {
+        let expected = new StringBuilder('Test');
+        expected.remove(2, 4);
+        expect(expected._stringArray.join('')).to.be.equal('Te');
+    })
+
+    it('Returns a string with all elements joined by an empty string (toString)', () => {
+        let expected = new StringBuilder('Test');
+        expected.append('Demo');
+        expect(expected.toString()).to.be.equal('TestDemo');
+    })
+
+    it('All passed in arguments should be strings. If any of them are not, throws a type error with the following message: "Argument must be a string"', () => {
+        let expected = new StringBuilder('Test');
+        expect(() => expected.append(2)).to.Throw(TypeError);
+        expect(() => expected.prepend(2)).to.Throw(TypeError);
+        expect(() => expected.insertAt(2, 5)).to.Throw(TypeError);
+        expect(() => new StringBuilder(2)).to.Throw(TypeError);
+    })
 })
