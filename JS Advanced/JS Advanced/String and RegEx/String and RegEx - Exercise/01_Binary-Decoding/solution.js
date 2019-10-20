2a49c916d5737759d75f588c97570b412a0e6dc1
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-20 10:41:09 +0300

    Bin Code Decoding Function + Mocha & Chai Tests

--- JS Advanced/JS Advanced/String and RegEx/String and RegEx - Exercise/01_Binary-Decoding/solution.js
+++ JS Advanced/JS Advanced/String and RegEx/String and RegEx - Exercise/01_Binary-Decoding/solution.js
@@ -8,6 +8,35 @@ function solve() {
 	
 }
 
-module.exports = function binDecode(binCode){
+function getBinCodeReduceNumber(binCode) {
+	while (binCode.toString().length > 1) {
+		binCode = binCode.toString().split('').reduce((a,b) => a + Number(b), 0)
+	}
+	return binCode
+}
 
+function binCodeReducer(binCode, number) {
+	return binCode.substring(number, binCode.length - number)
+}
+
+function binCodeReducerBySegments(binCode) {
+	return binCode.match(/.{8}/gim);
+}
+
+function parseBinToDecimal(binCodeArr) {
+	return binCodeArr.map(x => parseInt(x, 2));
+}
+
+function parseAsciiToText(arr) {
+	return arr.map(x => String.fromCharCode(x));
+}
+
+module.exports = function binDecode(binCode){
+	let binCodeReducerNumber = getBinCodeReduceNumber(binCode);
+	let reducedBinCode = binCodeReducer(binCode, binCodeReducerNumber);
+	let reduceBinCodeBySegments = binCodeReducerBySegments(reducedBinCode);
+	let decimalCode = parseBinToDecimal(reduceBinCodeBySegments);
+	let text = parseAsciiToText(decimalCode)
+
+	return text.join('')
 }
