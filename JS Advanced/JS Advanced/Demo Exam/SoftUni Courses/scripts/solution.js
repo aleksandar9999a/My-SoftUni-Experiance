697c08c8a4758e8dced4116112efb6a775b870f3
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-20 15:58:12 +0300

    Add and Remove Element from My Courses

--- JS Advanced/JS Advanced/Demo Exam/SoftUni Courses/scripts/solution.js
+++ JS Advanced/JS Advanced/Demo Exam/SoftUni Courses/scripts/solution.js
@@ -1,5 +1,56 @@
+const courses = {
+   'js-fundamentals': "JS Fundamentals - January",
+   'js-advanced': "JS Advanced - February",
+   'js-applications': "JS Applications - March",
+   'js-web': "JS Web - April"
+}
+
+function addElementToMyCourses() {
+   if (this.checked == true) {
+      addToMyCourses(this.value)
+   }else{
+      removeFromMyCourses(this.value)
+   }
+}
+
+function myEducationForm() {
+   console.log("gei");
+   
+}
+
+function addEventsToInputs(x) {
+   if (x.type === "checkbox") {
+      return x.addEventListener("click", addElementToMyCourses)
+   } else if(x.type === "radio"){
+      return x.addEventListener("click", myEducationForm)
+   }
+}
+
+function createElement(type, content) {
+   let el = document.createElement(type);
+   el.innerHTML = content;
+   return el;
+}
+
+function addToMyCourses(option) {
+   let ul = document.querySelector('#myCourses').getElementsByTagName('ul')[0];
+   let li = createElement('li', courses[option])
+   ul.appendChild(li);
+}
+
+function removeOption(option, element) {
+      if (element.innerHTML === courses[option]) {
+         element.remove()
+      }
+}
+function removeFromMyCourses(option) {
+   let li = document.querySelector('#myCourses').getElementsByTagName('li');
+   Array.from(li).map(removeOption.bind(undefined, option));
+   
+}
 function solve() {
-   // TODO: Add the functionality here.
+   Array.from(document.getElementsByTagName("input")).map(addEventsToInputs);
+   
 }
 
 solve();
