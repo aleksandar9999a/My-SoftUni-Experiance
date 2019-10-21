7421c89fcdbc0417fdb6f7c543bfcd1544ad195b
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-21 10:19:04 +0300

    Added tests for makeAnOrder

--- JS Advanced/JS Advanced/Demo Exam/PizzUni/PizzUni-test.js
+++ JS Advanced/JS Advanced/Demo Exam/PizzUni/PizzUni-test.js
@@ -1,37 +1,57 @@
 const { expect } = require('chai');
-const { assert } = require('chai');
-const pizzUni = require('./PizzUni');
+const PizzUni = require('./PizzUni');
 
 describe("Tests PizzUni", () => {
-    describe("An instance of PizzUni class should have 3 properties", () => {
-        it("PizzUni must return object", () => {
-            let expected = new pizzUni();
-            expect(expected).to.be.a("object");
-        })
-        it("PizzUni must have registeredUsers property", () => {
-            let expected = new pizzUni();
-            expect(expected).to.have.property("registeredUsers");
-        })
-        it("PizzUni must have availableProducts property", () => {
-            let expected = new pizzUni();
-            expect(expected).to.have.property("availableProducts");
-        })
-        it("PizzUni must have orders property", () => {
-            let expected = new pizzUni();
-            expect(expected).to.have.property("orders");
+    describe("Constructor", ()=>{
+        it("Must be instantiated without anything", () => {
+            let actual = new PizzUni();
+            expect(actual).to.be.a("object");
         })
     })
-
     describe("registerUser", ()=>{
-        it("Must return current object", () => {
-            let expected = new pizzUni();
-            expect(expected.registerUser("alex@gmail.com")).to.be.a("object");
+        it("If email exist throw error", () => {
+            let actual = new PizzUni();
+            actual.registerUser("alex@gmail.com");
+            expect(() => actual.registerUser("alex@gmail.com")).to.throw(Error, 'This email address (alex@gmail.com) is already being used!');
         })
-        it("Must return error if exist", () => {
-            let expected = new pizzUni();
-            expected.registerUser("alex@gmail.com");
-            let errF = () => expected.registerUser("alex@gmail.com");
-            assert(errF, `This email address (alex@gmail.com) is already being used!`)
+        it("If email doesn't exist then add to register", () => {
+            let actual = new PizzUni();
+            actual.registerUser("alex@gmail.com");
+            expect(actual.registeredUsers[0].email).to.be.equal("alex@gmail.com");
+        })
+    })
+    describe("makeAnOrder", () => {
+        it("If email is not registered throw error", () => {
+            let actual = new PizzUni();
+            expect(() => actual.makeAnOrder("alex@gmail.com", "Italian Style", "Fanta")).to.throw(Error, "You must be registered to make orders!");
+        })
+        it("If pizza doesn't exist throw error", () => {
+            let actual = new PizzUni();
+            actual.registerUser("alex@gmail.com");
+            expect(() => actual.makeAnOrder("alex@gmail.com", "Bulgarian Style", "Fanta")).to.throw(Error, "You must order at least 1 Pizza to finish the order.");
+        })
+        it("If data is valid return index of order", () => {
+            let actual = new PizzUni();
+            actual.registerUser("alex@gmail.com");
+            expect(actual.makeAnOrder("alex@gmail.com", "Italian Style", "Fanta")).to.be.equal(0)
+        })
+        it("Should return number of orders from orderHistory", () => {
+            let actual = new PizzUni();
+            actual.registerUser("alex@gmail.com");
+            actual.makeAnOrder("alex@gmail.com", "Italian Style", "Fanta");
+            expect(actual.registeredUsers[0].orderHistory.length).to.be.equal(1)
+        })
+        it("Should return the number of drinks in first orders", () => {
+            let actual = new PizzUni();
+            actual.registerUser("alex@gmail.com");
+            actual.makeAnOrder("alex@gmail.com", "Italian Style", "Fanta");
+            expect(actual.orders[0].orderedDrink[0].length).to.be.equal(1);
+        })
+        it("Should return order status pendind", () => {
+            let actual = new PizzUni();
+            actual.registerUser("alex@gmail.com");
+            actual.makeAnOrder("alex@gmail.com", "Italian Style", "Fanta");
+            expect(actual.orders[0].status).to.be.equal("pending");
         })
     })
 })
