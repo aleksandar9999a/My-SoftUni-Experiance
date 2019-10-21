5a078d3f7ca3afbbeb16f041f114b3cc48094e5c
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-21 15:33:14 +0300

    Added most of methods + mocha tests for them

--- JS Advanced/JS Advanced/Demo Exam/Computer/Computer-test.js
+++ JS Advanced/JS Advanced/Demo Exam/Computer/Computer-test.js
@@ -15,7 +15,45 @@ describe("Computer Tests", () => {
         it("Throw error if hddMemory is exceeded", () => {
             let actual = new Computer(4096, 7.5, 250000);
             let errF = () => actual.installAProgram('Word', 250001).
-            expect(errF).to.throw(Error, "There is not enough space on the hard drive");
+                expect(errF).to.throw(Error, "There is not enough space on the hard drive");
+        })
+    })
+    describe("uninstallAProgram", () => {
+        it("Return installedPrograms array without uninstalled program", () => {
+            let actual = new Computer(4096, 7.5, 250000);
+            actual.installAProgram('Word', 7300);
+            actual.uninstallAProgram('Word');
+            
+            expect(actual.installedPrograms[0]).to.be.equal(undefined)
+        })
+        it("Throw error if program not exist", () => {
+            let actual = new Computer(4096, 7.5, 250000);
+            let errF = () => actual.uninstallAProgram('Word');
+
+            expect(errF).to.throw(Error, "Control panel is not responding")
+        })
+    })
+
+    describe("openAProgram", () => {
+        it("Throw error if program in not present in installedPrograms", () => {
+            let actual = new Computer(4096, 7.5, 250000);
+            let errF = () => actual.openAProgram('Word');
+            expect(errF).to.throw(Error, "The Word is not recognized")
+        })
+        it("Throw error if program is already open", () => {
+            let actual = new Computer(4096, 7.5, 250000);
+            actual.installAProgram('Word', 7300);
+            actual.openAProgram('Word')
+            let errF = () => actual.openAProgram('Word');
+            expect(errF).to.throw(Error, "The Word is already open")
+        })
+    })
+
+    describe("taskManagerView", () => {
+        it("there is no opened program, the function returns All running smooth so far", () => {
+            let actual = new Computer(4096, 7.5, 250000);
+            expect(actual.taskManagerView()).to.be.equal("All running smooth so far")
         })
     })
 })
+
--- JS Advanced/JS Advanced/Demo Exam/Computer/Computer.js
+++ JS Advanced/JS Advanced/Demo Exam/Computer/Computer.js
@@ -11,8 +11,76 @@ class Computer {
         if (this.hddMemory < requiredSpace) {
             throw new Error("There is not enough space on the hard drive")
         }else{
-            this.installedPrograms.push({[name]: requiredSpace});
-            return {[name]: requiredSpace}
+            this.installedPrograms.push({name: name,
+                requiredSpace: requiredSpace});
+            this.hddMemory -= requiredSpace;
+            return {name: name,
+                requiredSpace: requiredSpace}
+        }
+    }
+
+    uninstallAProgram(name){
+        let res = this.isHere(name, this.installedPrograms);
+        if (!res[0]) {
+            throw new Error("Control panel is not responding")
+        }else{
+            let value = Object.values(this.installedPrograms[res[1]]);
+            this.hddMemory += value[0];
+            this.installedPrograms.splice(res[1], 1);
+        }
+
+        return this.installedPrograms
+    }
+    
+    isHere(programName, arr){
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].name === programName) {
+                return [true, i, arr[i]]
+            }
+        }
+
+        return [false];
+    }
+
+    openAProgram(name){
+        if (!this.isHere(name, this.installedPrograms)[0]) {
+            throw new Error(`The ${name} is not recognized`)
+        }
+
+        if (this.isHere(name, this.taskManager)[0]) {
+            throw new Error(`The ${name} is already open`)
+        }
+
+        let currProgramSpace = Object.values(this.isHere(name, this.installedPrograms)[2]);
+        let ram = (currProgramSpace[1] / this.ramMemory) * 1.5;
+        let cpu = ((currProgramSpace[1] / this.cpuGHz) / 500) * 1.5;
+        
+        this.taskManager.push( {name: name,
+            ramUsage: ram,
+            cpuUsage: cpu});
+
+        this.checkRam(name);
+
+        return {name: name,
+                ramUsage: ram,
+                cpuUsage: cpu}
+    }
+
+    checkRam(name){
+        let ram = this.taskManager.map(x => x.ramUsage).reduce((a, b) => a + b, 0);
+        if (ram >= 100) {
+            throw new Error(`${name} caused out of memory exception`)
+        }
+
+        let cpu = this.taskManager.map(x => x.cpuUsage).reduce((a, b) => a + b, 0);
+        if (cpu >= 100) {
+            throw new Error(`${name} caused out of cpu exception`)
+        }
+    }
+
+    taskManagerView(){
+        if (this.taskManager.length === 0) {
+            return "All running smooth so far";
         }
     }
 }
