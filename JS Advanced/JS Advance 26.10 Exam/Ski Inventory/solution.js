ca54abb1dce05f507767aa6d2f5e8b7cc25f4212
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-27 00:12:41 +0300

    Added all functions without filter

---
+++ JS Advanced/JS Advance 26.10 Exam/Ski Inventory/solution.js
@@ -0,0 +1,102 @@
+let availableProducts = [];
+let myProducts = [];
+
+function getCurrProductData(btn) {
+   let div = btn.parentElement.parentElement;
+   let name = div.getElementsByTagName('span')[0].innerHTML;
+   let price = div.getElementsByTagName('strong')[1].innerHTML;
+   return { name, price };
+}
+
+function generateLiFoMyProducts(product) {
+   let li = createElement('li', product.name);
+   let strong = createElement('strong', product.price);
+   appentElements(li, [strong]);
+   return li;
+}
+
+function showMyProducts() {
+   let list = document.getElementById('myProducts').getElementsByTagName('ul')[0];
+   list.innerHTML = '';
+   myProducts.map(generateLiFoMyProducts).map(x => list.appendChild(x));
+   let totalPrice =document.getElementsByTagName('h1')[1];
+   let price = myProducts.reduce((r, x) => r += Number(x.price), 0);
+   totalPrice.innerHTML = `Total Price: ${price.toFixed(2)}`;
+}
+
+function moveToMyProducts() {
+   let currProduct = getCurrProductData(this);
+   myProducts.push(currProduct);
+   availableProducts.filter(x => x.name === currProduct.name).map(x => x.quantity--);
+   
+   showAvailableProducts();
+   showMyProducts();
+}
+
+function getProductData() {
+   let values = document.getElementsByTagName('input');
+   let name = values[1].value;
+   let quantity = values[2].value;
+   let price = values[3].value;
+
+   if (name !== '' && quantity !== '' && price !== '' && quantity > 0) {
+      return { name, quantity, price };
+   }
+
+   return 'invalid'
+}
+
+function createElement(type, content) {
+   let e = document.createElement(type);
+   if (typeof content === "string") {
+      e.innerHTML = content;
+   }
+   if (typeof content === "object") {
+      e.appendChild(content);
+   }
+
+   return e;
+}
+
+function appentElements(parent, elements) {
+   elements.map(x => parent.appendChild(x));
+   return parent;
+}
+
+function genarateLiForAvProducts(product) {
+   let li = createElement('li');
+   let span = createElement('span', product.name);
+   let strong = createElement('strong', `Available: ${product.quantity}`);
+   let div = createElement('div');
+   let strong2 = createElement('strong', Number(product.price).toFixed(2));
+   let btn = createElement('button', "Add to Client's List");
+   btn.addEventListener('click', moveToMyProducts)
+
+   appentElements(div, [strong2, btn]);
+   appentElements(li, [span, strong, div]);
+
+   return li;
+}
+
+function showAvailableProducts() {
+   let list = document.getElementById('products').getElementsByTagName('ul')[0];
+   list.innerHTML = '';
+   availableProducts.filter(x => x.quantity > 0).map(genarateLiForAvProducts).map(x => list.appendChild(x));
+}
+
+function init(e) {
+   let productData = getProductData();
+   if (productData !== 'invalid') {
+      availableProducts.push(productData);
+   }
+   e.preventDefault()
+   showAvailableProducts();
+}
+
+function clearMyProducts() {
+   myProducts = [];
+   showMyProducts();
+}
+
+document.getElementsByTagName('button')[1].addEventListener('click', init);
+document.getElementById('myProducts').getElementsByTagName('button')[0].addEventListener('click', clearMyProducts)
