97cd29bf34c9b8f739dc05a45c8b49059884cd6d
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-28 10:26:19 +0200

    Added functions who create new item, new element and appends elements

--- JS Advanced/JS Advanced/Demo Exam/Form Acceptance/scripts/app.js
+++ JS Advanced/JS Advanced/Demo Exam/Form Acceptance/scripts/app.js
@@ -8,13 +8,44 @@ function acceptance() {
 		return true;
 	}
 
+	function createElement(type, content) {
+		let e = document.createElement(type);
+		if (typeof content === "string") {
+			e.innerHTML = content;
+		}
+		if (typeof content === "object") {
+			e.appendChild(content);
+		}
+
+		return e;
+	}
+
+	function appendElements(parent, elements) {
+		if (Array.isArray(elements)) {
+			return elements.map(x => parent.appendChild(x));
+		}
+		return parent.appendChild(elements);
+	}
+
+	function createItem(company, product, quantity, scrape) {
+		let div = createElement('div');
+		let p = createElement('p', `[${company}] ${product} - ${quantity - scrape} pieces`);
+		let btn = createElement('button', 'Out of stock');
+		btn.type = 'button';
+
+		appendElements(div, [p, btn]);
+		return div;
+	}
+
 	function init() {
 		let [company, product, quantity, scrape] = document.getElementsByTagName('input');
+		
 		if (validateInput(company.value, product.value, quantity.value, scrape.value)) {
-			console.log('pedal');
-			
+			let newItem = createItem(company.value, product.value, quantity.value, scrape.value);
+			let section = document.getElementById('warehouse');
+			appendElements(section, newItem);
 		}
 	}
-	
+
 	document.getElementById('acceptance').addEventListener('click', init)
 }
