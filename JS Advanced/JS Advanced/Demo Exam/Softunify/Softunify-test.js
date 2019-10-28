f270d65ae8fe9a6a00106c248c6424224f5d0909
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-28 13:59:20 +0200

    First Test for properties

--- JS Advanced/JS Advanced/Demo Exam/Softunify/Softunify-test.js
+++ JS Advanced/JS Advanced/Demo Exam/Softunify/Softunify-test.js
@@ -1,2 +1,11 @@
 const { expect } = require('chai');
 const SoftUniFy = require('./SoftUniFy');
+
+describe('SoftUniFy Tests', () => {
+    describe('Properties test', () => {
+        it('Should contain allSongs property that is initialized as an empty object', () => {
+            let actual = new SoftUniFy();
+            expect(actual.allSongs).to.be.a('object');
+        })
+    })
+})
