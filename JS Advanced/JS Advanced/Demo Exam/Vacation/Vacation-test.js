9b94606cf39a7972054dac964c4f340db0189397
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-28 11:11:05 +0200

    Added constructor and test for values

--- JS Advanced/JS Advanced/Demo Exam/Vacation/Vacation-test.js
+++ JS Advanced/JS Advanced/Demo Exam/Vacation/Vacation-test.js
@@ -1,2 +1,17 @@
 const { expect } = require('chai');
 const Vacation = require('./Vacation');
+
+describe('Vacation Tests', () => {
+    describe('4 properties - kids with default value', () => {
+        it('Test default value for kids', () => {
+            let actual = new Vacation('Mr Pesho', 'San diego', 2000);
+            expect(actual.kids).to.be.a('object');
+        })
+        it('Others values test', () => {
+            let actual = new Vacation('Mr Pesho', 'San diego', 2000);
+            expect(actual.organizer).to.be.equal('Mr Pesho');
+            expect(actual.destination).to.be.equal('San diego');
+            expect(actual.budget).to.be.equal(2000);
+        })
+    })
+})
--- JS Advanced/JS Advanced/Demo Exam/Vacation/Vacation.js
+++ JS Advanced/JS Advanced/Demo Exam/Vacation/Vacation.js
@@ -1,5 +1,10 @@
 class Vacation{
-
+    constructor(organizer, destination, budget){
+        this.organizer = organizer;
+        this.destination = destination;
+        this.budget = budget;
+        this.kids = {};
+    }
 }
 
 module.exports = Vacation;
