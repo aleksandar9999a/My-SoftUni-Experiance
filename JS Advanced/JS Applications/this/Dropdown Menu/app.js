53e54b8eea4c55c82b0d2b7437d769f77b866479
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-28 21:15:08 +0200

    Refactoring Dropdown Menu

--- JS Advanced/JS Applications/this/Dropdown Menu/app.js
+++ JS Advanced/JS Applications/this/Dropdown Menu/app.js
@@ -1,3 +1,46 @@
+function app(ul, box, dropDown, listItemClassMatcher) {
+    return {
+        ul: ul,
+        box: box,
+        dropDown: dropDown,
+
+        handleEvent: function (e) {
+            if (typeof this[e.target.id] === "function") {
+                this[e.target.id]()
+            }
+
+            if (listItemClassMatcher) {
+                this.deep(e.target.textContent)
+            }
+
+        },
+
+        setBoxColor: function (color) {
+            this.box.style.backgroundColor = color;
+        },
+
+        dropdown: function () {
+            if (this.ul.style.display === "block") {
+                this.ul.style.display = "none";
+                this.setBoxColor("")
+            } else {
+                this.ul.style.display = "block"
+            }
+        },
+
+        deep: function (color) {
+            this.box.style.backgroundColor = color;
+        }
+    }
+}
+
 function solve() {
-    // ToDo
+    document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('click', app(
+            document.getElementById('dropdown-ul'),
+            document.getElementById('box'),
+            document.getElementById('dropdown'),
+            domNode => domNode.classList.value.match('deep')
+        ));
+    })
 }
