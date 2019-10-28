f484b91cd998a7e3a0e4676900a8c7cc62903ae1
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-28 10:15:24 +0200

    validateInput

--- JS Advanced/JS Advanced/Demo Exam/Form Acceptance/scripts/app.js
+++ JS Advanced/JS Advanced/Demo Exam/Form Acceptance/scripts/app.js
@@ -1,11 +1,19 @@
 function acceptance() {
 
+	function validateInput(company, product, quantity, scrape) {
+		if (company === '' || product === '' || quantity === '' || scrape === '' || isNaN(quantity) || isNaN(scrape)) {
+			return false;
+		}
+
+		return true;
+	}
 
 	function init() {
 		let [company, product, quantity, scrape] = document.getElementsByTagName('input');
-		console.log(company, product, quantity, scrape);
-		
-		
+		if (validateInput(company.value, product.value, quantity.value, scrape.value)) {
+			console.log('pedal');
+			
+		}
 	}
 	
 	document.getElementById('acceptance').addEventListener('click', init)
