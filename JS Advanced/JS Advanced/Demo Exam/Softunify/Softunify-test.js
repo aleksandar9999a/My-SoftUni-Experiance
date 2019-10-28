e5e6002ae37208093681d727fafb114f08d0566f
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-28 14:28:13 +0200

    downloadSong and playSong tests

--- JS Advanced/JS Advanced/Demo Exam/Softunify/Softunify-test.js
+++ JS Advanced/JS Advanced/Demo Exam/Softunify/Softunify-test.js
@@ -8,4 +8,23 @@ describe('SoftUniFy Tests', () => {
             expect(actual.allSongs).to.be.a('object');
         })
     })
+
+    describe('downloadSong', () => {
+        it('Adds the given information to the allSongs', () => {
+            let actual = new SoftUniFy();
+            actual.downloadSong('Eminem', 'Venom', 'Knock, Knock...')
+            expect(actual.allSongs['Eminem']).to.eql({rate: 0, votes: 0, songs: ['Venom - Knock, Knock...']});
+        })
+    })
+    describe('playSong', () => {
+        it('Searches all already downloaded songs and returns them', () => {
+            let actual = new SoftUniFy();
+            actual.downloadSong('Eminem', 'Venom', 'Knock, Knock...');
+            expect(actual.playSong('Venom')).to.be.equal("Eminem:\nVenom - Knock, Knock...\n")
+        })
+        it('Return string if song is not found', () => {
+            let actual = new SoftUniFy();
+            expect(actual.playSong('Venom')).to.be.equal("You have not downloaded a Venom song yet. Use SoftUniFy's function downloadSong() to change that!")
+        })
+    })
 })
