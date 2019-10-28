130bed27597dbccd58920d78579e4db9e63385db
aleksandar9999a <aleksandar9999a@gmail.com>
2019-10-28 11:29:06 +0200

    registerChild and tests

--- JS Advanced/JS Advanced/Demo Exam/Vacation/Vacation-test.js
+++ JS Advanced/JS Advanced/Demo Exam/Vacation/Vacation-test.js
@@ -19,7 +19,17 @@ describe('Vacation Tests', () => {
         it('If the budget is not enough throw error', () => {
             let actual = new Vacation('Mr Pesho', 'San diego', 2000);
             let errF = () => actual.registerChild('Ivan', '5', 1500);
-            expect(errF).to.throw(Error, "Ivan's money is not enough to go on vacation to San diego.")
+            expect(errF).to.throw(Error, "Ivan's money is not enough to go on vacation to San diego.");
+        })
+        it('If current kid is already into that grade the method should return string', () => {
+            let actual = new Vacation('Mr Pesho', 'San diego', 2000);
+            actual.registerChild('Ivan', '5', 20000);
+            expect(actual.registerChild('Ivan', '5', 20000)).to.be.equal('Ivan is already in the list for this San diego vacation.');
+        })
+        it('Expect to have one kid in kids propertie', () => {
+            let actual = new Vacation('Mr Pesho', 'San diego', 2000);
+            actual.registerChild('Ivan', '5', 20000);
+            expect(actual.kids[5]).to.be.have.length(1);
         })
     })
 })
--- JS Advanced/JS Advanced/Demo Exam/Vacation/Vacation.js
+++ JS Advanced/JS Advanced/Demo Exam/Vacation/Vacation.js
@@ -1,15 +1,33 @@
-class Vacation{
-    constructor(organizer, destination, budget){
+class Vacation {
+    constructor(organizer, destination, budget) {
         this.organizer = organizer;
         this.destination = destination;
         this.budget = budget;
         this.kids = {};
     }
 
-    registerChild(name, grade, budget){
+    isRegistered(name, grade) {
+        if (this.kids[grade]) {
+            return this.kids[grade].filter(x => x.includes(name)).length === 1;
+        }
+        return false
+    }
+
+    registerChild(name, grade, budget) {
         if (budget < this.budget) {
             throw new Error(`${name}'s money is not enough to go on vacation to ${this.destination}.`)
         }
+
+        if (this.isRegistered(name, grade)) {
+            return `${name} is already in the list for this ${this.destination} vacation.`
+        }
+
+        if (this.kids[grade]) {
+            this.kids[grade].push(`${name}-${budget}`);
+        } else {
+            this.kids[grade] = [`${name}-${budget}`]
+        }
+
     }
 }
 
