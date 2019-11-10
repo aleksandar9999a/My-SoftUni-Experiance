350261c1097c438b520e3569ffa6dc16cad12194
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-10 12:08:14 +0200

    Bus Schedule New Solution

--- JS Advanced/JS Applications/REST Services and AJAX/02.BUS-SCHEDULE/app.js
+++ JS Advanced/JS Applications/REST Services and AJAX/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,53 @@
 function solve() {
+    let state = {
+        name: '',
+        next: 'depot'
+    }
 
     function depart() {
-        console.log('Depart TODO...');
+        const BASE_URL = `https://judgetests.firebaseio.com/schedule/${state.next}.json`;
+        const resultDiv = document.getElementsByClassName('info')[0];
+        const departBtn = document.getElementById('depart');
+        const arriveBtn = document.getElementById('arrive');
+
+        fetch(BASE_URL)
+            .then(parseResult)
+            .then(({ name, next }) => {
+                setState(name, next);
+                showResult(resultDiv, `Next stop ${state.name}`);
+                buttonActivation(departBtn, true);
+                buttonActivation(arriveBtn, false);
+            })
+            .catch(res => showResult(resultDiv, 'Error'));
+    }
+
+    function buttonActivation(btn, status) {
+        btn.disabled = status;
+    }
+
+    function showResult(resultDiv, result) {
+        resultDiv.innerHTML = result;
+    }
+
+    function setState(name, next) {
+        state = {
+            name,
+            next
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        const resultDiv = document.getElementsByClassName('info')[0];
+        const departBtn = document.getElementById('depart');
+        const arriveBtn = document.getElementById('arrive');
+
+        showResult(resultDiv, `Arriving at ${state.name}`);
+        buttonActivation(departBtn, false);
+        buttonActivation(arriveBtn, true);
+    }
+
+    function parseResult(result) {
+        return result.json()
     }
 
     return {
