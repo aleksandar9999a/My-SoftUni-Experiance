a247d57141d6adf3000c22f05e14c6f61fe1017d
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-13 20:41:04 +0200

    Update fetchData

--- JS Advanced/JS Applications/Asynchronous Programming/Blog/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming/Blog/app.js
@@ -7,14 +7,14 @@ function handleError(x) {
 
 function deserializeData(x) { return x.json() }
 
-function fetchData() {
-    return fetch('https://blog-apps-c12bf.firebaseio.com/.json')
+function fetchData(handleError = handleError, deserializeData = deserializeData, url) {
+    return fetch(url)
         .then(handleError)
         .then(deserializeData)
         .catch(console.error)
 }
 
-
+const BASE_URL = 'https://blog-apps-c12bf.firebaseio.com/.json';
 
 function attachEvents() {
 
