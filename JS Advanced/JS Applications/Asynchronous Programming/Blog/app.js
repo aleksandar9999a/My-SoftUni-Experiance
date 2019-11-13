1152962fe6367d64fd6876681602847965c24c9d
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-13 22:11:50 +0200

    Added functions who find, filter, create and appends comments and details for post

--- JS Advanced/JS Applications/Asynchronous Programming/Blog/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming/Blog/app.js
@@ -3,20 +3,18 @@ import { fetchData } from './data.js';
 const BASE_URL = 'https://blog-apps-c12bf.firebaseio.com/';
 const makeUrl = x => `${BASE_URL}${x}.json`;
 
-const getPosts = fetchData.bind(undefined, undefined, undefined, makeUrl('posts'));
+const getPosts = () => fetchData(undefined, undefined, makeUrl('posts'));
 const getPost = id => fetchData(undefined, undefined, makeUrl(`posts/${id}`));
-window.getComments = fetchData.bind(undefined, undefined, undefined, makeUrl('comments'));
+const getComments = () => fetchData(undefined, undefined, makeUrl('comments'));
 
 function displayPosts(posts) {
     let fragment = document.createDocumentFragment();
-
     Object.keys(posts).forEach(x => {
         let option = document.createElement('option');
         option.value = x;
         option.innerHTML = posts[x].title;
         fragment.appendChild(option);
     });
-
     html.posts().appendChild(fragment);
 }
 
@@ -26,12 +24,47 @@ const actions = {
     },
     btnViewPost: async () => {
         const post = await getPost(html.posts().value);
-        Object.keys(post).forEach(x => {
-            if (typeof html[x] === 'function') {
-                html[x]().innerHTML = post[x];
-            }
-        });
+        const comments = await getComments();
+
+        createComments(comments, post);
+        createPost(post);
     }
+}
+
+function createPost(post) {
+    Object.keys(post).forEach(x => {
+        if (typeof html[x] === 'function') {
+            html[x]().innerHTML = post[x];
+        }
+    });
+}
+
+function createComments(comments, post) {
+    Object
+        .keys(comments)
+        .filter(filterById(comments, post))
+        .map(getCommentById(comments))
+        .map(createLiElement)
+        .map(addToCommentBody);
+}
+
+function addToCommentBody(x) {
+    html.comments().innerHTML = '';
+    html.comments().appendChild(x);
+}
+
+function createLiElement(x) {
+    let li = document.createElement('li');
+    li.innerHTML = x.text;
+    return li;
+}
+
+function getCommentById(comments) {
+    return x => comments[x];
+}
+
+function filterById(comments, post) {
+    return x => comments[x].postId === post.id;
 }
 
 function handleEvent(e) {
