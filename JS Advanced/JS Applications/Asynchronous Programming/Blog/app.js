08d62b7c9df701650ca5bc70036a65ddfc9eb406
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-13 20:54:03 +0200

    Export FetchData in file

--- JS Advanced/JS Applications/Asynchronous Programming/Blog/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming/Blog/app.js
@@ -1,24 +1,11 @@
-function handleError(x) {
-    if (!x.ok) {
-        throw new Error(x.statusText);
-    }
-    return x;
-}
-
-function deserializeData(x) { return x.json() }
-
-function fetchData(hError = handleError, dData = deserializeData, url) {
-    return fetch(url)
-        .then(hError)
-        .then(dData)
-        .catch(console.error);
-}
+import { fetchData } from './data.js';
 
 const BASE_URL = 'https://blog-apps-c12bf.firebaseio.com/';
-const makeUrl = x => `${BASE_URL}${x}.json`
-const getPosts = fetchData.bind(undefined, undefined, undefined, makeUrl('posts'));
-const getPost = id => fetchData(undefined, undefined, makeUrl(`posts/${id}`));
-const getComments = fetchData.bind(undefined, undefined, undefined, makeUrl('comments'));
+const makeUrl = x => `${BASE_URL}${x}.json`;
+
+window.getPosts = fetchData.bind(undefined, undefined, undefined, makeUrl('posts'));
+window.getPost = id => fetchData(undefined, undefined, makeUrl(`posts/${id}`));
+window.getComments = fetchData.bind(undefined, undefined, undefined, makeUrl('comments'));
 
 
 function attachEvents() {
