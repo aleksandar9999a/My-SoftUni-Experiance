53caa852c9052007dc6dd2d3f7df26d1d8ff693e
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-13 20:39:21 +0200

    Added fetchData function

--- JS Advanced/JS Applications/Asynchronous Programming/Blog/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming/Blog/app.js
@@ -1,5 +1,23 @@
+function handleError(x) {
+    if (!x.ok) {
+        throw new Error(x.statusText);
+    }
+    return x;
+}
+
+function deserializeData(x) { return x.json() }
+
+function fetchData() {
+    return fetch('https://blog-apps-c12bf.firebaseio.com/.json')
+        .then(handleError)
+        .then(deserializeData)
+        .catch(console.error)
+}
+
+
+
 function attachEvents() {
-    console.log('TODO...');
+
 }
 
 attachEvents();
