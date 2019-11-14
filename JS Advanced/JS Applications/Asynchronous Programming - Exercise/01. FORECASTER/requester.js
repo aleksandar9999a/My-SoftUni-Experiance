fed68349f9202759c06cddf5da99d10526bbd665
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-14 13:01:26 +0200

    Added getRequester

--- JS Advanced/JS Applications/Asynchronous Programming - Exercise/01. FORECASTER/requester.js
+++ JS Advanced/JS Applications/Asynchronous Programming - Exercise/01. FORECASTER/requester.js
@@ -1,1 +1,16 @@
+function handleError(x) {
+    if (!x.ok) {
+        throw new Error(x.statusText);
+    }
+    return x;
+}
+function deserializeData(x) {
+    return x.json();
+}
 
+export function getRequester(hError = handleError, dData = deserializeData, url) {
+    return fetch(url)
+        .then(hError)
+        .then(dData)
+        .catch(console.error);
+}
