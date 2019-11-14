0a9062df522ee531c4a2e5cec281bdd447d111bf
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-14 13:06:03 +0200

    Added handle event function and action map

--- JS Advanced/JS Applications/Asynchronous Programming - Exercise/01. FORECASTER/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming - Exercise/01. FORECASTER/app.js
@@ -1,1 +1,20 @@
-const url = 'https://judgetests.firebaseio.com/locations.json';
+import { getRequester } from './requester.js';
+const BASE_URL = 'https://judgetests.firebaseio.com/';
+
+const weatherApp = {
+    handleEvent: function (e) {
+        if (typeof action[e.target.id] === 'function') {
+            action[e.target.id]()
+        }
+    }
+}
+
+const action = {
+    submit: function () {
+        console.log('work');
+        
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('click', weatherApp)
+})
