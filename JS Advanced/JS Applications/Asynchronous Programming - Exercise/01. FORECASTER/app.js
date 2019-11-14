d6f5109e3b73d58faef96e93bf2c476da63d6fb4
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-14 14:44:54 +0200

    Added Functions who get weather for current location

--- JS Advanced/JS Applications/Asynchronous Programming - Exercise/01. FORECASTER/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming - Exercise/01. FORECASTER/app.js
@@ -1,20 +1,50 @@
 import { getRequester } from './requester.js';
 const BASE_URL = 'https://judgetests.firebaseio.com/';
 
-const weatherApp = {
-    handleEvent: function (e) {
-        if (typeof action[e.target.id] === 'function') {
-            action[e.target.id]()
-        }
+function handleEvent(e) {
+    if (typeof action[e.target.id] === 'function') {
+        action[e.target.id]()
     }
 }
 
+function searchCode(locationData) {
+    const input = document.getElementById('location');
+    let currData = locationData.find(x => x.name = input.value);
+
+    if (!currData) {
+        throw new Error('Location does not exist');
+    }
+
+    return currData;
+}
+
+function displayTodayWeather(data) {
+    console.log(data);
+    
+}
+
+function displayUpcomingWeather(data) {
+    console.log(data);
+}
+
 const action = {
-    submit: function () {
-        console.log('work');
-        
+    submit: async function () {
+        let currData = searchCode(await getLocation());
+        let todayWeather = await getTodayWeather.bind(undefined, currData.code)()();
+        let upcomingWeather = await getUpcomingWeather.bind(undefined, currData.code)()();
+        displayTodayWeather(todayWeather);
+        displayUpcomingWeather(upcomingWeather);
     }
 }
+
+function makeURL(extension) {
+    return BASE_URL + extension;
+}
+
+const getLocation = getRequester.bind(undefined, undefined, undefined, makeURL('locations.json'));
+const getTodayWeather = code => getRequester.bind(undefined, undefined, undefined, makeURL(`forecast/today/${code}.json`));
+const getUpcomingWeather = code => getRequester.bind(undefined, undefined, undefined, makeURL(`forecast/upcoming/${code}.json`));
+
 document.addEventListener('DOMContentLoaded', function () {
-    document.addEventListener('click', weatherApp)
+    document.addEventListener('click', handleEvent)
 })
