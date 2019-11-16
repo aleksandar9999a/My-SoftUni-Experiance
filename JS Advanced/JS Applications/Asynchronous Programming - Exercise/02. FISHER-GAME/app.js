023f22d6b546b182b792a8db11091b19ca276a61
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-16 22:30:16 +0200

    Added addEventListener and action map

--- JS Advanced/JS Applications/Asynchronous Programming - Exercise/02. FISHER-GAME/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming - Exercise/02. FISHER-GAME/app.js
@@ -1,1 +1,20 @@
-import requester from './components/Requester.js'
+import requester from './components/Requester.js';
+
+const action = {
+    'load': function(){
+        
+    },
+    'add': '',
+    'update': '',
+    'delete': '',
+}
+
+function handleEvent(e){
+    if (typeof action[e.target.id] === 'function') {
+        action[e.target.id]();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.addEventListener('click', handleEvent)
+})
