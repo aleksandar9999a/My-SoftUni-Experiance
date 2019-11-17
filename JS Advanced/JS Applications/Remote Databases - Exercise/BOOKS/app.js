1fc12f36f6941f3e48875600f59570f8e962af85
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-17 12:22:34 +0200

    Added handleEvent and action map

--- JS Advanced/JS Applications/Remote Databases - Exercise/BOOKS/app.js
+++ JS Advanced/JS Applications/Remote Databases - Exercise/BOOKS/app.js
@@ -1,1 +1,21 @@
-console.log('My requests...')
+import requester from './requester.js';
+
+
+
+const action = {
+    'submit': function(){
+        console.log('pedal');
+        
+    }
+}
+
+function handleEvent(e) {
+    e.preventDefault();
+    if (typeof action[e.target.id] === 'function') {
+        action[e.target.id]();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('click', handleEvent)
+})
