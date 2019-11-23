3c3454e4f1c98d28d40d43c3488fbf7ad5b55486
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-24 00:06:25 +0200

    Render first page

--- JS Advanced/JS Applications/Routing/Team-Manager-Skeleton/scripts/app.js
+++ JS Advanced/JS Applications/Routing/Team-Manager-Skeleton/scripts/app.js
@@ -1,1 +1,24 @@
- // TODO
+(() => {
+    const partials = {
+        header: './templates/common/header.hbs',
+        footer: './templates/common/footer.hbs'
+    };
+
+    const app = Sammy('#main', function () {
+        this.use('Handlebars', 'hbs');
+
+        this.get('index.html', function (ctx) {
+            this.loadPartials(partials).then(function(){
+                this.partial('./templates/home/home.hbs')
+            });
+        })
+
+        this.get('/', function (ctx) {
+            this.loadPartials(partials).then(function(){
+                this.partial('./templates/home/home.hbs')
+            });
+        })
+    });
+
+    app.run();
+})()
