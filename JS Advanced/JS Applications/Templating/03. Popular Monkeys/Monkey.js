2c17843aeae659c37f31b9d754c0fb0ba3d9939f
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-23 22:45:59 +0200

    Solution

--- JS Advanced/JS Applications/Templating/03. Popular Monkeys/Monkey.js
+++ JS Advanced/JS Applications/Templating/03. Popular Monkeys/Monkey.js
@@ -1,8 +1,8 @@
 export default class Monkey {
-    constructor(id, name, img, info) {
+    constructor({id, name, image, info}) {
         this.id = id;
         this.name = name;
-        this.img = img;
+        this.img = image;
         this.info = info;
     }
 
@@ -30,17 +30,34 @@ export default class Monkey {
         }
     }
 
+    showInfo() {
+        const myId = this.dataset.id;
+        const p = document.getElementById(myId);
+
+        if (p.style.display === 'none') {
+            p.style.display = 'block';
+        } else {
+            p.style.display = 'none';
+        }
+    }
+
     render() {
         const div = this.createElement('div', '', 'monkey');
         const h2 = this.createElement('h2', this.name);
+
         const img = this.createElement('img');
         img.src = this.img;
+
         const btn = this.createElement('button', 'Info');
         btn.dataset.id = this.id;
+        btn.addEventListener('click', this.showInfo)
+
         const p = this.createElement('p', this.info);
         p.id = this.id;
+        p.style.display = 'none';
 
         this.appendElements(div, [h2, img, btn, p]);
+        
         return div;
     }
 }
--- JS Advanced/JS Applications/Templating/03. Popular Monkeys/render.js
+++ JS Advanced/JS Applications/Templating/03. Popular Monkeys/render.js
@@ -1,2 +1,13 @@
 import monkeys from './monkeys.js';
 import Monkey from './Monkey.js';
+
+function createMonkey(data) {
+    return new Monkey(data).render();
+}
+
+function appendElementtoMonkeyDiv(e) {
+    const root = document.getElementsByClassName('monkeys')[0];
+    root.appendChild(e);
+}
+
+monkeys.map(createMonkey).forEach(appendElementtoMonkeyDiv);
