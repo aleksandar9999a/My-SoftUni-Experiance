1ac8627ba0b15692351bb96342f4720fc8148776
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-24 00:16:49 +0200

    Added render for about page

--- JS Advanced/JS Applications/Routing/Team-Manager-Skeleton/scripts/app.js
+++ JS Advanced/JS Applications/Routing/Team-Manager-Skeleton/scripts/app.js
@@ -7,18 +7,30 @@
     const app = Sammy('#main', function () {
         this.use('Handlebars', 'hbs');
 
-        this.get('index.html', function (ctx) {
-            this.loadPartials(partials).then(function(){
-                this.partial('./templates/home/home.hbs')
-            });
-        })
+        this.get('#/', loadHome);
+        this.get('#/home', loadHome);
 
-        this.get('/', function (ctx) {
+        this.get('#/about', function (ctx) {
+            addHeaderInfo(ctx);
             this.loadPartials(partials).then(function(){
-                this.partial('./templates/home/home.hbs')
+                this.partial('./templates/about/about.hbs')
             });
         })
     });
 
+    function addHeaderInfo(ctx){
+        ctx.loggedIn = true;
+        ctx.username = 'Alexandar';
+    }
+
+    function loadHome(ctx) {
+        addHeaderInfo(ctx);
+        this.loadPartials(partials).then(function(){
+            this.partial('./templates/home/home.hbs')
+        });
+    }
+
     app.run();
 })()
+
+// sessionStorage.getItem('authtoken') !== null
