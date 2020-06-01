de1a835cb5d2c2ddb64448d8dfe11bfd9605c20b
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-01 19:48:56 +0300

    Refactoring

--- JavaScript Fundamentals May 2019/Arrays/Add or Subtract.js
+++ JavaScript Fundamentals May 2019/Arrays/Add or Subtract.js
@@ -1,26 +1,9 @@
-function addOrSubtract(numbersArray) {
-    let number = 0;
-    let newArrayFromOld = [];
-    let oldSum = 0;
-    let newSum = 0;
-
-    for (let i = 0; i < numbersArray.length; i++) {
-        number = Number(numbersArray[i]);
-        oldSum += number;
-
-        if (number % 2 == 0) {
-            number += i;
-            newArrayFromOld.push(number);
-        }
-        else{
-            number -= i;
-            newArrayFromOld.push(number);
-        }  
-        newSum += number;
-    }
-
-    console.log(newArrayFromOld);
-    console.log(oldSum);
-    console.log(newSum);
+function addOrSubtract(arr) {
+    const updatedArr = arr.map((x, i) => x % 2 === 0 ? x + i : x - i);
+    const sumOfArr = arr.reduce((acc, x) => acc+=x);
+    const sumOfUpdated = updatedArr.reduce((acc, x) => acc+=x);
+    console.log(updatedArr);
+    console.log(sumOfArr);
+    console.log(sumOfUpdated);
 }
 addOrSubtract([5, 15, 23, 56, 35])
